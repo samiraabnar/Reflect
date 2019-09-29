f07b191a0be97aa6e4c249efee007178eba862c7
Samira Abnar <samiraabnar@gmail.com>
2019-09-29 17:41:16 +0200

    add repeat for data

--- keras_trainer.py
+++ keras_trainer.py
@@ -60,7 +60,7 @@ if __name__ == '__main__':
   initial_learning_rate = 0.1
   lr_schedule = tf.keras.optimizers.schedules.ExponentialDecay(
     initial_learning_rate,
-    decay_steps=100000,
+    decay_steps=1000,
     decay_rate=0.96,
     staircase=True)
 
