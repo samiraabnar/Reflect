fix masking

tf2_models/lm_lstm.py
@@ -18,9 +18,6 @@ class LmLSTM(tf.keras.Model):
 
     self.create_vars()
 
-    self.build(input_shape=(None, None))
-    self.summary()
-
   @tf.function
   def create_vars(self):
     self.input_embedding = tf.compat.v2.keras.layers.Embedding(input_dim=self.hparams.input_dim,
@@ -51,11 +48,14 @@ class LmLSTM(tf.keras.Model):
     embedded_input = self.input_embedding_dropout(self.input_embedding(inputs))
     rnn_outputs = embedded_input
 
+
+    input_mask = tf.cast(self.input_embedding.compute_mask(inputs), dtype=tf.float32)
     for i in np.arange(self.hparams.depth):
-      rnn_outputs, state_h, state_c = self.stacked_rnns[i](rnn_outputs)
+      rnn_outputs, state_h, state_c = self.stacked_rnns[i](rnn_outputs, mask=input_mask)
 
     rnn_outputs = self.output_embedding_dropout(rnn_outputs)
     logits = self.output_embedding(rnn_outputs)
+    logits = logits * input_mask[...,None]
 
     return logits
 

tf2_models/metrics.py
@@ -1,28 +1,32 @@
 import tensorflow as tf
 
 @tf.function
-def masked_sequence_loss(logits, targets, padding_symbol=0):
-  sequence_mask = tf.cast(targets != padding_symbol, dtype=tf.float32)
-  return tf.compat.v2.nn.sparse_softmax_cross_entropy_with_logits(logits=logits,
-                                                                  labels=targets,
+def masked_sequence_loss(y_true, y_pred, padding_symbol=0):
+  print(y_pred.shape)
+  print(y_true.shape)
+  y_true = tf.cast(tf.squeeze(y_true), tf.int32)
+  sequence_mask = tf.cast(y_true != padding_symbol, dtype=tf.float32)
+  print(sequence_mask.shape)
+  return tf.compat.v2.nn.sparse_softmax_cross_entropy_with_logits(logits=y_pred,
+                                                                  labels=y_true,
                                                                   name='loss') * sequence_mask
 @tf.function
-def accuracy(logits, targets, padding_symbol=0):
+def accuracy(targets, logits, padding_symbol=0):
   sequence_mask = tf.cast(targets != padding_symbol, dtype=tf.float32)
-  return accuracy_topk(logits, targets, sequence_mask, topk=1)
+  return accuracy_topk(targets, logits, sequence_mask, topk=1)
 
 @tf.function
-def accuracy_top2(logits, targets, padding_symbol=0):
+def accuracy_top2(targets, logits, padding_symbol=0):
   sequence_mask = tf.cast(targets != padding_symbol, dtype=tf.float32)
-  return accuracy_topk(logits, targets, sequence_mask, topk=2)
+  return accuracy_topk(targets, logits, sequence_mask, topk=2)
 
 @tf.function
-def accuracy_top5(logits, targets, padding_symbol=0):
+def accuracy_top5(targets, logits, padding_symbol=0):
   sequence_mask = tf.cast(targets != padding_symbol, dtype=tf.float32)
-  return accuracy_topk(logits, targets, sequence_mask, topk=5)
+  return accuracy_topk(targets, logits, sequence_mask, topk=5)
 
 @tf.function
-def accuracy_topk(logits, targets, sequence_mask, topk):
+def accuracy_topk(targets, logits, sequence_mask, topk):
   orig_shape = tf.shape(logits)
   last_dim = orig_shape[-1]
   logits = tf.reshape(logits, (-1,last_dim))

tf2_models/trainer.py
@@ -26,10 +26,16 @@ class Trainer(object):
     self.ckpt = tf.train.Checkpoint(step=tf.Variable(1), optimizer=self.optimizer, net=self.model)
     self.manager = tf.train.CheckpointManager(self.ckpt, ckpt_dir, max_to_keep=2)
 
+
+    x, y = iter(self.task.valid_dataset).next()
+    model(x)
+    model.summary()
+
     model.compile(
       optimizer=self.optimizer,
-      loss=self.task.get_loss_fn(), #tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+      loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
       metrics=['accuracy'])
+
 
     summary_dir = os.path.join(log_dir, 'summaries')
     tf.io.gfile.makedirs(log_dir)