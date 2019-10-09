d70f8458e7f90976e22f93cf43803b2a8281ed6d
Samira Abnar <samiraabnar@gmail.com>
2019-10-09 11:04:08 +0200

    remove dropout from output

--- tf2_models/lm_lstm.py
+++ tf2_models/lm_lstm.py
@@ -139,7 +139,7 @@ class LmLSTMSharedEmb(tf.keras.Model):
       rnn_outputs, state_h, state_c = self.stacked_rnns[i](rnn_outputs, mask=input_mask,
                                                            training=training)
 
-    rnn_outputs = self.output_embedding_dropout(rnn_outputs,training=training)
+    #rnn_outputs = self.output_embedding_dropout(rnn_outputs,training=training)
     logits = self.input_embedding(rnn_outputs, mode='linear')
     logits = logits * float_input_mask[...,None] + tf.eye(self.hparams.output_dim)[0] * (1 - float_input_mask[...,None])
 
