79b04512320fe89a69aeab8d6774441eec32a61e
Samira Abnar <samiraabnar@gmail.com>
2019-10-17 14:02:04 +0200

    set l2 for cl lstm

--- tf2_models/lm_lstm.py
+++ tf2_models/lm_lstm.py
@@ -25,7 +25,7 @@ class LmLSTM(tf.keras.Model):
                          'indrop-'+str(self.hparams.input_dropout_rate)])
 
     self.regularizer = tf.keras.regularizers.l1_l2(l1=0.00,
-                                                   l2=0.00000)
+                                                   l2=0.00001)
     self.create_vars()
 
   @tf.function
