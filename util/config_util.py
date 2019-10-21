09cfc227d99e96d3ff0a42ccaecdc23377127ea4
Samira Abnar <samiraabnar@gmail.com>
2019-10-21 10:44:31 +0200

    new config

--- util/config_util.py
+++ util/config_util.py
@@ -95,6 +95,13 @@ radam_fast_long = {
 'hold_base_rate_steps': 1000000
 }
 
+radam_slow_long = {
+'learning_rate': 0.0001,
+'optimizer': 'radam',
+'hold_base_rate_steps': 1000000
+}
+
+
 radam_fast = {
 'learning_rate': 0.001,
 'optimizer': 'radam',
@@ -105,5 +112,6 @@ TRAIN_PARAMS = {'radam_slow': radam_slow,
                 'radam_fast': radam_fast,
                 'adam_slow':  adam_slow,
                 'radam_fast_long': radam_fast_long,
+                'radam_slow_long': radam_slow_long,
                 'adam_mid': adam_mid,
                 'adam_midmid': adam_midmid}
