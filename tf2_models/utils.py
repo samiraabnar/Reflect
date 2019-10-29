afefb6361119d81bc69dec83f8147b9f6836b185
Samira Abnar <samiraabnar@gmail.com>
2019-10-29 14:41:27 +0100

    fix offline distill

--- tf2_models/utils.py
+++ tf2_models/utils.py
@@ -5,6 +5,6 @@ def camel2snake(name):
   return name[0].lower() + re.sub(r'(?!^)[A-Z]', lambda x: '_' + x.group(0).lower(), name[1:])
 
 def log_summary(log_value, log_name, summary_scope):
-  tf.summary.scalar(name=log_name, tensor=log_value, family=summary_scope)
+  tf.summary.scalar(log_name, log_value, family=summary_scope)
 
 
