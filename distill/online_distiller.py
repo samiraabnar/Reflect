d77a9aa29a4a0eaab16fdf59bd6d7efd063f2040
Samira Abnar <samiraabnar@gmail.com>
2019-10-31 16:26:01 +0100

    add distill configs

--- distill/online_distiller.py
+++ distill/online_distiller.py
@@ -125,7 +125,7 @@ class OnlineDistiller(Distiller):
         logits = self.student_model(x)
         distill_loss = self.student_model.loss(y_pred=logits, y_true=y)
         reg_loss = tf.math.add_n(self.student_model.losses)
-        actual_loss = self.task.task_loss(y_pred=logits, y_true=y_true)
+        actual_loss = self.task_loss(y_pred=logits, y_true=y_true)
         final_loss = scale_distill_grads * self.distill_params.student_distill_rate * distill_loss + \
                      self.distill_params.student_gold_rate * actual_loss + reg_loss
       grads = tape.gradient(final_loss, self.student_model.trainable_weights)
