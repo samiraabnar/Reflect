ddca322e1d3ba8e036e393071433fef9d4792621
Samira Abnar <samiraabnar@gmail.com>
2019-11-04 21:48:23 +0100

    update lm lstm shared emb

--- tf2_models/trainer.py
+++ tf2_models/trainer.py
@@ -41,6 +41,7 @@ class Trainer(object):
 
     self.callbacks = [ckpt_callback, summary_callback]
 
+  @tf.funtion
   def get_lr_schedule(self):
     if self.train_params.optimizer == 'radam':
       initial_learning_rate = self.train_params.learning_rate
