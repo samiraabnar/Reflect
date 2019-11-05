737705e2893ca39440b41a330e800b41dd20b4ad
Samira Abnar <samiraabnar@gmail.com>
2019-11-05 13:01:20 +0100

    lstm lm shared embd without learning init

--- tf2_models/lm_lstm.py
+++ tf2_models/lm_lstm.py
@@ -267,6 +267,82 @@ class LmLSTMSharedEmb(tf.keras.Model):
     return _call(inputs, padding_symbol, **kwargs)
 
 
+class LmLSTMSharedEmbV2(tf.keras.Model):
+
+  def __init__(self, hparams, scope="lm_lstm_shared_emb",*inputs, **kwargs):
+    del kwargs['cl_token']
+    super(LmLSTMSharedEmbV2, self).__init__()
+    self.hparams = hparams
+    self.scope = scope
+
+    self.model_name = '_'.join([self.scope,
+                         'em-'+str(self.hparams.embedding_dim),
+                         'h-'+str(self.hparams.hidden_dim),
+                         'd-'+str(self.hparams.depth),
+                         'hdrop-'+str(self.hparams.hidden_dropout_rate),
+                         'indrop-'+str(self.hparams.input_dropout_rate)])
+
+    self.regularizer = tf.keras.regularizers.l1_l2(l1=0.00,
+                                                   l2=0.0000)
+    self.create_vars()
+
+  def create_vars(self):
+
+    @tf.function
+    def _create_vars():
+      self.input_embedding = SharedEmbeddings(vocab_size=self.hparams.input_dim,
+                                              hidden_size=self.hparams.embedding_dim,
+                                              initializer_range=self.hparams.initializer_range,
+                                              regularizer=self.regularizer,
+                                              name='embedding')
+      self.input_embedding_dropout = tf.keras.layers.Dropout(self.hparams.input_dropout_rate)
+      self.output_embedding_dropout = tf.keras.layers.Dropout(self.hparams.hidden_dropout_rate)
+      initializer_range = self.hparams.embedding_dim ** -0.5 if self.hparams.initializer_range is None else self.hparams.initializer_range
+      self.output_projection = tf.keras.layers.Dense(units=self.hparams.embedding_dim,
+                                                     kernel_initializer=get_initializer(initializer_range))
+
+      self.stacked_rnns = []
+      self.rnn_initial_states = []
+      for _ in np.arange(self.hparams.depth):
+        initializer_range = self.hparams.hidden_dim ** -0.5 if self.hparams.initializer_range is None else self.hparams.initializer_range
+        self.stacked_rnns.append(tf.keras.layers.LSTM(units=self.hparams.hidden_dim,
+                                                      return_sequences=True,
+                                                      return_state=True,
+                                                      go_backwards=False,
+                                                      stateful=False,
+                                                      unroll=False,
+                                                      time_major=False,
+                                                      recurrent_dropout=self.hparams.hidden_dropout_rate,
+                                                      dropout=self.hparams.hidden_dropout_rate,
+                                                      kernel_regularizer=self.regularizer,
+                                                      recurrent_regularizer=self.regularizer,
+                                                      bias_regularizer=self.regularizer,
+                                                      kernel_initializer=get_initializer(initializer_range),
+                                                      recurrent_initializer=get_initializer(initializer_range)
+                                                      ))
+
+    _create_vars()
+
+
+  def call(self, inputs, padding_symbol=0, **kwargs):
+    @tf.function(experimental_relax_shapes=True)
+    def _call(inputs, padding_symbol, **kwargs):
+      input_mask = tf.cast(inputs != padding_symbol, dtype=tf.bool)
+      embedded_input = self.input_embedding_dropout(self.input_embedding(inputs, mode='embedding'),
+                                                    **kwargs)
+      rnn_outputs = embedded_input
+      for i in np.arange(self.hparams.depth):
+        rnn_outputs, state_h, state_c = self.stacked_rnns[i](rnn_outputs, mask=input_mask,
+                                                             **kwargs)
+
+      rnn_outputs = self.output_projection(rnn_outputs, **kwargs)
+      rnn_outputs = self.output_embedding_dropout(rnn_outputs,**kwargs)
+      logits = self.input_embedding(rnn_outputs, mode='linear')
+
+      return logits
+
+    return _call(inputs, padding_symbol, **kwargs)
+
 
 if __name__ == '__main__':
   class hparams(object):
--- util/models.py
+++ util/models.py
@@ -1,10 +1,10 @@
-from tf2_models.lm_lstm import LmLSTM, LmLSTMSharedEmb, ClassifierLSTM
+from tf2_models.lm_lstm import LmLSTM, LmLSTMSharedEmb, ClassifierLSTM, LmLSTMSharedEmbV2
 from tf2_models.lm_transformer import LmGPT2, LmGPT2SharedWeights, ClassifierGPT2, ClassifierGPT2SharedWeights
 
 MODELS = {"lm_lstm": LmLSTM,
           "lm_gpt2": LmGPT2,
           "lm_gpt2_shared": LmGPT2SharedWeights,
-          "lm_lstm_shared_emb": LmLSTMSharedEmb,
+          "lm_lstm_shared_emb": LmLSTMSharedEmbV2,
           'cl_gpt2': ClassifierGPT2,
           'cl_lstm': ClassifierLSTM,
           'cl_gpt2_shared': ClassifierGPT2SharedWeights}
