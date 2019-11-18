da9248aae84be8f563938663d98da5648dd55e47
Samira Abnar <samiraabnar@gmail.com>
2019-11-18 23:58:47 +0100

    fast online run

--- util/distill_params.py
+++ util/distill_params.py
@@ -96,10 +96,27 @@ pure_distill_5 = {
 'teacher_optimizer' : 'radam'
 }
 
+pure_distill_4_fastonline = {
+'distill_temp' : 1.0,
+'student_distill_rate' : 1.0,
+'student_gold_rate' : 0.0,
+'student_learning_rate' : 0.0001,
+'student_decay_steps' : 10000,
+'student_hold_base_rate_steps' :  1000,
+'student_warmup_steps' : 10000,
+'student_optimizer' : 'adam',
+'teacher_learning_rate' : 0.001,
+'teacher_decay_steps' : 10000,
+'teacher_warmup_steps' : 0,
+'teacher_hold_base_rate_steps' : 0,
+'teacher_optimizer' : 'radam'
+}
+
 DISTILL_PARAMS = {'pure_distill_1' :  pure_distill_1,
                   'pure_distill_2' :  pure_distill_2,
                   'pure_distill_3' :  pure_distill_3,
                   'pure_distill_4': pure_distill_4,
                   'pure_distill_4_radamfast': pure_distill_4_radamfast,
-                  'pure_distill_5': pure_distill_5
+                  'pure_distill_5': pure_distill_5,
+                  'pure_distill_4_fastonline': pure_distill_4_fastonline
                   }
