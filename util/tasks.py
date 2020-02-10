ae2387470dbd8cce67de1eaba80b1352134ed88c
Samira Abnar <samiraabnar@gmail.com>
2020-02-10 11:54:13 +0100

    mnist

--- util/tasks.py
+++ util/tasks.py
@@ -1,7 +1,9 @@
+from tasks.mnist import Mnist
 from tasks.sv_agreement import SvAgreementLM, WordSvAgreementLM, WordSvAgreementVP
 
 TASKS = {
   'sv_agreement_lm': SvAgreementLM,
   'word_sv_agreement_lm': WordSvAgreementLM,
-  'word_sv_agreement_vp': WordSvAgreementVP
+  'word_sv_agreement_vp': WordSvAgreementVP,
+  'mnist': Mnist,
 }
