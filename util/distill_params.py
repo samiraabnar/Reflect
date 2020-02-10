ef158f1bd0cb025104287618df4b03b8c735d382
Samira Abnar <samiraabnar@gmail.com>
2020-02-10 20:17:59 +0100

    mnist

--- util/distill_params.py
+++ util/distill_params.py
@@ -217,6 +217,22 @@ pure_dstl_4_fstonln = {
 'teacher_optimizer' : 'radam'
 }
 
+pure_dstl_mn_fstonln = {
+'distill_temp' : 1.0,
+'student_distill_rate' : 1.0,
+'student_gold_rate' : 0.0,
+'student_learning_rate' : 0.001,
+'student_decay_steps' : 10000,
+'student_hold_base_rate_steps' :  1000,
+'student_warmup_steps' : 10000,
+'student_optimizer' : 'adam',
+'teacher_learning_rate' : 0.001,
+'teacher_decay_steps' : 10000,
+'teacher_warmup_steps' : 0,
+'teacher_hold_base_rate_steps' : 0,
+'teacher_optimizer' : 'radam'
+}
+
 DISTILL_PARAMS = {'pure_dstl_1' :  pure_dstl_1,
                   'pure_dstl_2' :  pure_dstl_2,
                   'pure_dstl_3' :  pure_dstl_3,
