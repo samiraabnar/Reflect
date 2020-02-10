a04f8c2aabda2df1b25214955e8cc75ea2e61cb3
Samira Abnar <samiraabnar@gmail.com>
2020-02-10 22:50:20 +0100

    add capsnet

--- tf2_models/capnet.py
+++ tf2_models/capnet.py
@@ -2,47 +2,67 @@ import tensorflow as tf
 from tensorflow.keras import initializers, layers, models
 import tensorflow.keras.backend as K
 
+
 class Length(layers.Layer):
   """
-  Compute the length of vectors. This is used to compute a Tensor that has the same shape with y_true in margin_loss
-  inputs: shape=[dim_1, ..., dim_{n-1}, dim_n]
-  output: shape=[dim_1, ..., dim_{n-1}]
+  Compute the length of vectors. This is used to compute a Tensor that has the same shape with y_true in margin_loss.
+  Using this layer as model's output can directly predict labels by using `y_pred = np.argmax(model.predict(x), 1)`
+  inputs: shape=[None, num_vectors, dim_vector]
+  output: shape=[None, num_vectors]
   """
 
   def call(self, inputs, **kwargs):
-    return tf.math.sqrt(tf.reduce_sum(tf.math.square(inputs), -1))
+    return K.sqrt(K.sum(K.square(inputs), -1) + K.epsilon())
 
   def compute_output_shape(self, input_shape):
     return input_shape[:-1]
 
+  def get_config(self):
+    config = super(Length, self).get_config()
+    return config
+
 
 class Mask(layers.Layer):
   """
-  Mask a Tensor with shape=[None, d1, d2] by the max value in axis=1.
-  Output shape: [None, d2]
+  Mask a Tensor with shape=[None, num_capsule, dim_vector] either by the capsule with max length or by an additional
+  input mask. Except the max-length capsule (or specified capsule), all vectors are masked to zeros. Then flatten the
+  masked Tensor.
+  For example:
+      ```
+      x = keras.layers.Input(shape=[8, 3, 2])  # batch_size=8, each sample contains 3 capsules with dim_vector=2
+      y = keras.layers.Input(shape=[8, 3])  # True labels. 8 samples, 3 classes, one-hot coding.
+      out = Mask()(x)  # out.shape=[8, 6]
+      # or
+      out2 = Mask()([x, y])  # out2.shape=[8,6]. Masked with true labels y. Of course y can also be manipulated.
+      ```
   """
 
   def call(self, inputs, **kwargs):
-    # use true label to select target capsule, shape=[batch_size, num_capsule]
-    if type(inputs) is list:  # true label is provided with shape = [batch_size, n_classes], i.e. one-hot code.
+    if type(inputs) is list:  # true label is provided with shape = [None, n_classes], i.e. one-hot code.
       assert len(inputs) == 2
       inputs, mask = inputs
-    else:  # if no true label, mask by the max length of vectors of capsules
-      x = inputs
-      # Enlarge the range of values in x to make max(new_x)=1 and others < 0
-      x = (x - tf.reduce_max(x, 1, True)) / tf.epsilon() + 1
-      mask = tf.clip(x, 0, 1)  # the max value in x clipped to 1 and other to 0
+    else:  # if no true label, mask by the max length of capsules. Mainly used for prediction
+      # compute lengths of capsules
+      x = K.sqrt(K.sum(K.square(inputs), -1))
+      # generate the mask which is a one-hot code.
+      # mask.shape=[None, n_classes]=[None, num_capsule]
+      mask = K.one_hot(indices=K.argmax(x, 1), num_classes=x.get_shape().as_list()[1])
 
-    # masked inputs, shape = [batch_size, dim_vector]
-    inputs_masked = tf.keras.backend.batch_dot(inputs, mask, [1, 1])
-    return inputs_masked
+    # inputs.shape=[None, num_capsule, dim_capsule]
+    # mask.shape=[None, num_capsule]
+    # masked.shape=[None, num_capsule * dim_capsule]
+    masked = K.batch_flatten(inputs * K.expand_dims(mask, -1))
+    return masked
 
   def compute_output_shape(self, input_shape):
     if type(input_shape[0]) is tuple:  # true label provided
-      return tuple([None, input_shape[0][-1]])
-    else:
-      return tuple([None, input_shape[-1]])
+      return tuple([None, input_shape[0][1] * input_shape[0][2]])
+    else:  # no true label provided
+      return tuple([None, input_shape[1] * input_shape[2]])
 
+  def get_config(self):
+    config = super(Mask, self).get_config()
+    return config
 
 def squash(vectors, axis=-1):
   """
