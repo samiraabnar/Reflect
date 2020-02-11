77ef336cc24144c6838829306fa2c0b9943c09c7
Samira Abnar <samiraabnar@gmail.com>
2020-02-11 12:29:14 +0100

    add capsnet

--- tasks/mnist.py
+++ tasks/mnist.py
@@ -117,45 +117,4 @@ class AffNist(Task):
   def convert_examples(self, examples):
     return tf.cast(examples['image'], dtype=tf.float32), tf.cast(examples['label'], dtype=tf.int32)
 
-  def setup_datasets(self):
-    self.info = self.databuilder.info
-    self.n_train_batches = int(
-      self.info.splits['train'].num_examples / self.task_params.batch_size)
-    self.n_test_batches = int(
-      self.info.splits['test'].num_examples / self.task_params.batch_size)
-    self.n_valid_batches = int(
-      self.info.splits['test'].num_examples / self.task_params.batch_size)
-
-    self.databuilder.download_and_prepare(download_dir=self.data_dir)
-
-    self.test_dataset = self.databuilder.as_dataset(split="test")
-    assert isinstance(self.test_dataset, tf.data.Dataset)
-    self.test_dataset = self.test_dataset.map(map_func=lambda x: self.convert_examples(x),
-                                              num_parallel_calls=tf.data.experimental.AUTOTUNE)
-    self.test_dataset = self.test_dataset.repeat()
-    self.test_dataset = self.test_dataset.batch(
-      batch_size=self.task_params.batch_size)
-    self.test_dataset = self.test_dataset.prefetch(
-      tf.data.experimental.AUTOTUNE)
-
-    self.train_dataset = self.databuilder.as_dataset(split="train")
-    assert isinstance(self.train_dataset, tf.data.Dataset)
-    self.train_dataset = self.train_dataset.map(map_func=lambda x: self.convert_examples(x),
-                                                num_parallel_calls=tf.data.experimental.AUTOTUNE)
-    self.train_dataset = self.train_dataset.repeat()
-    self.train_dataset = self.train_dataset.shuffle(1024)
-    self.train_dataset = self.train_dataset.batch(
-      batch_size=self.task_params.batch_size)
-    # self.train_dataset = self.train_dataset.cache()
-    self.train_dataset = self.train_dataset.prefetch(
-      tf.data.experimental.AUTOTUNE)
-
-    self.valid_dataset = self.databuilder.as_dataset(split="test")
-    assert isinstance(self.valid_dataset, tf.data.Dataset)
-    self.valid_dataset = self.valid_dataset.map(map_func=lambda x: self.convert_examples(x),
-                                              num_parallel_calls=tf.data.experimental.AUTOTUNE)
-    self.valid_dataset = self.valid_dataset.repeat()
-    self.valid_dataset = self.valid_dataset.batch(
-      batch_size=self.task_params.batch_size)
-    self.valid_dataset = self.valid_dataset.prefetch(
-      tf.data.experimental.AUTOTUNE)
+  
