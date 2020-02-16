389b00d4ce3e1a2c6cd4c7609579bc0c708c8b4d
Samira Abnar <samiraabnar@gmail.com>
2020-02-16 17:08:21 +0100

    add sst2

--- tasks/sst.py
+++ tasks/sst.py
@@ -39,8 +39,8 @@ class SST2(Task):
     return ([None],[])
 
   def convert_examples(self, examples):
-    print(examples)
-    return self.text_encoder.encode(examples['sentence'].numpy()), examples['label']
+    print(examples['sentence'[0]])
+    return self.text_encoder.encode(examples['sentence'][0].numpy()), examples['label']
 
 
 
