aa9769ca8894e9aa997f677fb4beb1fb016f4878
Samira Abnar <samiraabnar@gmail.com>
2020-03-06 22:09:43 +0100

    new job gc

---
+++ gscripts/gc_online_dstl_lm_biglstm.sh
@@ -0,0 +1,71 @@
+#!/usr/bin/env bash
+
+conda activate indist
+
+cd ~/Codes/InDist
+
+# LSTM 2 LSTM
+
+CUDA_VISIBLE_DEVICES=0 python distill/distill_main.py  --task=word_sv_agreement_lm \
+--teacher_model=lm_lstm_shared_emb \
+--student_model=lm_lstm_shared_emb \
+--student_exp_name=lisa_rep_std1 \
+--teacher_exp_name=lisa_rep_tchr1 \
+--teacher_config=biglstm_drop31_v2 \
+--student_config=very_big_gpt_v10 \
+--distill_mode=online \
+--distill_config=dstl5_910_crs_slwfst_2 &
+
+CUDA_VISIBLE_DEVICES=1 python distill/distill_main.py  --task=word_sv_agreement_lm \
+--teacher_model=lm_lstm_shared_emb \
+--student_model=lm_lstm_shared_emb \
+--student_exp_name=lisa_rep_std10 \
+--teacher_exp_name=lisa_rep_tchr10 \
+--teacher_config=biglstm_drop31_v2 \
+--student_config=very_big_gpt_v10 \
+--distill_mode=online \
+--distill_config=dstl_910_crs_slwfst_2 &
+
+CUDA_VISIBLE_DEVICES=2 python distill/distill_main.py  --task=word_sv_agreement_lm \
+--teacher_model=lm_lstm_shared_emb \
+--student_model=lm_lstm_shared_emb \
+--student_exp_name=lisa_rep_std20 \
+--teacher_exp_name=lisa_rep_tchr20 \
+--teacher_config=biglstm_drop31_v2 \
+--student_config=biglstm_drop31_v2 \
+--distill_mode=online \
+--distill_config=dstl01_910_crs_slwfst_2 &
+
+# LSTM 2 Transformer
+
+CUDA_VISIBLE_DEVICES=3 python distill/distill_main.py  --task=word_sv_agreement_lm \
+--teacher_model=lm_lstm_shared_emb \
+--student_model=lm_gpt2 \
+--student_exp_name=lisa_rep_std1 \
+--teacher_exp_name=lisa_rep_tchr1 \
+--teacher_config=biglstm_drop31_v2 \
+--student_config=very_big_gpt_v10 \
+--distill_mode=online \
+--distill_config=dstl5_910_crs_slwfst_2 &
+
+CUDA_VISIBLE_DEVICES=4 python distill/distill_main.py  --task=word_sv_agreement_lm \
+--teacher_model=lm_lstm_shared_emb \
+--student_model=lm_gpt2 \
+--student_exp_name=lisa_rep_std10 \
+--teacher_exp_name=lisa_rep_tchr10 \
+--teacher_config=biglstm_drop31_v2 \
+--student_config=very_big_gpt_v10 \
+--distill_mode=online \
+--distill_config=dstl_910_crs_slwfst_2 &
+
+CUDA_VISIBLE_DEVICES=5 python distill/distill_main.py  --task=word_sv_agreement_lm \
+--teacher_model=lm_lstm_shared_emb \
+--student_model=lm_gpt2 \
+--student_exp_name=lisa_rep_std20 \
+--teacher_exp_name=lisa_rep_tchr20 \
+--teacher_config=biglstm_drop31_v2 \
+--student_config=very_big_gpt_v10 \
+--distill_mode=online \
+--distill_config=dstl01_910_crs_slwfst_2 &
+
+wait
--- util/distill_params.py
+++ util/distill_params.py
@@ -342,6 +342,43 @@ rpdst5_019_crs_slwfst_2 = {
 'schedule': 'crs_fst'
 }
 
+rpdst01_019_crs_slwfst_2 = {
+'distill_temp' : 0.01,
+'student_distill_rate' : 0.0,
+'student_gold_rate' : 0.1,
+'student_distill_rep_rate': 0.9,
+'student_learning_rate' : 0.0001,
+'student_decay_steps' : 10000,
+'student_hold_base_rate_steps' :  0,
+'student_warmup_steps' : 0,
+'student_optimizer' : 'adam',
+'teacher_learning_rate' : 0.001,
+'teacher_decay_steps' : 10000,
+'teacher_warmup_steps' : 10000,
+'teacher_hold_base_rate_steps' : 0,
+'teacher_optimizer' : 'radam',
+'schedule': 'crs_fst'
+}
+
+
+dstl01_910_crs_slwfst_2 = {
+'distill_temp' : 0.01,
+'student_distill_rate' : 0.9,
+'student_gold_rate' : 0.1,
+'student_distill_rep_rate': 0.0,
+'student_learning_rate' : 0.0001,
+'student_decay_steps' : 10000,
+'student_hold_base_rate_steps' :  0,
+'student_warmup_steps' : 0,
+'student_optimizer' : 'adam',
+'teacher_learning_rate' : 0.001,
+'teacher_decay_steps' : 10000,
+'teacher_warmup_steps' : 10000,
+'teacher_hold_base_rate_steps' : 0,
+'teacher_optimizer' : 'radam',
+'schedule': 'crs_fst'
+}
+
 DISTILL_PARAMS = {'pure_dstl_1' :  pure_dstl_1,
                   'pure_dstl_2' :  pure_dstl_2,
                   'pure_dstl_3' :  pure_dstl_3,
@@ -361,4 +398,6 @@ DISTILL_PARAMS = {'pure_dstl_1' :  pure_dstl_1,
                   'dstl_910_crs_slwfst_2': dstl_910_crs_slwfst_2,
                   'dstl5_910_crs_slwfst_2': dstl5_910_crs_slwfst_2,
                   'rpdst5_019_crs_slwfst_2': rpdst5_019_crs_slwfst_2,
+                  'dstl01_910_crs_slwfst_2': dstl01_910_crs_slwfst_2,
+                  'rpdst01_019_crs_slwfst_2': rpdst01_019_crs_slwfst_2
                   }
