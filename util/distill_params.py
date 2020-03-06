638ff26fbd7f5162367b9da7ce39bb024dfc7438
Samira Abnar <samiraabnar@gmail.com>
2020-03-07 00:26:28 +0100

    schedule distillation

--- util/distill_params.py
+++ util/distill_params.py
@@ -399,6 +399,26 @@ schdexp_dstl_910_crs_slwfst_2 = {
 'schedule': 'crs_fst'
 }
 
+schdexp_dstl5_910_crs_slwfst_2 = {
+'distill_temp' : 5.0,
+'student_distill_rate' : 1.0,
+'distill_min_rate': 0.0,
+'distill_schedule': 'exp',
+'student_gold_rate' : 0.1,
+'student_distill_rep_rate': 0.0,
+'student_learning_rate' : 0.0001,
+'student_decay_steps' : 10000,
+'student_hold_base_rate_steps' :  0,
+'student_warmup_steps' : 0,
+'student_optimizer' : 'adam',
+'teacher_learning_rate' : 0.001,
+'teacher_decay_steps' : 10000,
+'teacher_warmup_steps' : 10000,
+'teacher_hold_base_rate_steps' : 0,
+'teacher_optimizer' : 'radam',
+'schedule': 'crs_fst'
+}
+
 DISTILL_PARAMS = {'pure_dstl_1' :  pure_dstl_1,
                   'pure_dstl_2' :  pure_dstl_2,
                   'pure_dstl_3' :  pure_dstl_3,
@@ -419,5 +439,7 @@ DISTILL_PARAMS = {'pure_dstl_1' :  pure_dstl_1,
                   'dstl5_910_crs_slwfst_2': dstl5_910_crs_slwfst_2,
                   'rpdst5_019_crs_slwfst_2': rpdst5_019_crs_slwfst_2,
                   'dstl01_910_crs_slwfst_2': dstl01_910_crs_slwfst_2,
-                  'rpdst01_019_crs_slwfst_2': rpdst01_019_crs_slwfst_2
+                  'rpdst01_019_crs_slwfst_2': rpdst01_019_crs_slwfst_2,
+                  'schdexp_dstl5_910_crs_slwfst_2': schdexp_dstl5_910_crs_slwfst_2,
+                  'schdexp_dstl_910_crs_slwfst_2': schdexp_dstl_910_crs_slwfst_2
                   }
