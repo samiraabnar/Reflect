39cf3245d9d8d68a670eeff97cb9a7e9d16419ab
Samira Abnar <samiraabnar@gmail.com>
2020-03-07 16:23:25 +0100

    new runs

--- gscripts/gc_offline_schd_dstl_lm_biglstm.sh
+++ gscripts/gc_offline_schd_dstl_lm_biglstm.sh
@@ -14,8 +14,8 @@ CUDA_VISIBLE_DEVICES=0 python distill/distill_main.py  --task=word_sv_agreement_
 --teacher_exp_name=gc_tmp5_rep_tchr1 \
 --teacher_config=biglstm_drop31_v2 \
 --student_config=very_big_gpt_v10 \
---distill_mode=online \
---distill_config=dstl5_910_crs_slwfst_2 > run0 &
+--distill_mode=off_schdld \
+--distill_config=schdexp_dstl5_910_crs_slwfst_2 > run0 &
 
 CUDA_VISIBLE_DEVICES=1 python distill/distill_main.py  --task=word_sv_agreement_lm \
 --teacher_model=lm_lstm_shared_emb \
@@ -24,8 +24,8 @@ CUDA_VISIBLE_DEVICES=1 python distill/distill_main.py  --task=word_sv_agreement_
 --teacher_exp_name=gc_tmp1_rep_tchr10 \
 --teacher_config=biglstm_drop31_v2 \
 --student_config=very_big_gpt_v10 \
---distill_mode=online \
---distill_config=dstl_910_crs_slwfst_2 > run1 &
+--distill_mode=off_schdld \
+--distill_config=schdexp_dstl_910_crs_slwfst_2 > run1 &
 
 CUDA_VISIBLE_DEVICES=2 python distill/distill_main.py  --task=word_sv_agreement_lm \
 --teacher_model=lm_lstm_shared_emb \
@@ -34,8 +34,8 @@ CUDA_VISIBLE_DEVICES=2 python distill/distill_main.py  --task=word_sv_agreement_
 --teacher_exp_name=gc_tmp01_rep_tchr20 \
 --teacher_config=biglstm_drop31_v2 \
 --student_config=biglstm_drop31_v2 \
---distill_mode=online \
---distill_config=dstl01_910_crs_slwfst_2 > run2 &
+--distill_mode=off_schdld \
+--distill_config=schdexp_dstl01_910_crs_slwfst_2 > run2 &
 
 # LSTM 2 Transformer
 
@@ -46,8 +46,8 @@ CUDA_VISIBLE_DEVICES=3 python distill/distill_main.py  --task=word_sv_agreement_
 --teacher_exp_name=gc_tmp5_rep_tchr1 \
 --teacher_config=biglstm_drop31_v2 \
 --student_config=very_big_gpt_v10 \
---distill_mode=online \
---distill_config=dstl5_910_crs_slwfst_2 > run3 &
+--distill_mode=off_schdld \
+--distill_config=schdexp_dstl5_910_crs_slwfst_2 > run3 &
 
 CUDA_VISIBLE_DEVICES=4 python distill/distill_main.py  --task=word_sv_agreement_lm \
 --teacher_model=lm_lstm_shared_emb \
@@ -56,8 +56,8 @@ CUDA_VISIBLE_DEVICES=4 python distill/distill_main.py  --task=word_sv_agreement_
 --teacher_exp_name=gc_tmp1_rep_tchr10 \
 --teacher_config=biglstm_drop31_v2 \
 --student_config=very_big_gpt_v10 \
---distill_mode=online \
---distill_config=dstl_910_crs_slwfst_2 > run4 &
+--distill_mode=off_schdld \
+--distill_config=schdexp_dstl_910_crs_slwfst_2 > run4 &
 
 CUDA_VISIBLE_DEVICES=5 python distill/distill_main.py  --task=word_sv_agreement_lm \
 --teacher_model=lm_lstm_shared_emb \
@@ -66,7 +66,7 @@ CUDA_VISIBLE_DEVICES=5 python distill/distill_main.py  --task=word_sv_agreement_
 --teacher_exp_name=gc_tmp01_rep_tchr20 \
 --teacher_config=biglstm_drop31_v2 \
 --student_config=very_big_gpt_v10 \
---distill_mode=online \
---distill_config=dstl01_910_crs_slwfst_2  > run5&
+--distill_mode=off_schdld \
+--distill_config=schdexp_dstl01_910_crs_slwfst_2  > run5&
 
 wait
--- util/distill_params.py
+++ util/distill_params.py
@@ -379,6 +379,27 @@ dstl01_910_crs_slwfst_2 = {
 'schedule': 'crs_fst'
 }
 
+schdexp_dstl01_910_crs_slwfst_2 = {
+'distill_temp' : 0.01,
+'student_distill_rate' : 1.0,
+'distill_min_rate': 0.0,
+'distill_schedule': 'exp',
+'student_gold_rate' : 0.1,
+'student_distill_rep_rate': 0.0,
+'student_learning_rate' : 0.0001,
+'student_decay_steps' : 10000,
+'student_hold_base_rate_steps' :  0,
+'student_warmup_steps' : 0,
+'student_optimizer' : 'adam',
+'teacher_learning_rate' : 0.001,
+'teacher_decay_steps' : 10000,
+'teacher_warmup_steps' : 10000,
+'teacher_hold_base_rate_steps' : 0,
+'teacher_optimizer' : 'radam',
+'schedule': 'crs_fst'
+}
+
+
 schdexp_dstl_910_crs_slwfst_2 = {
 'distill_temp' : 1.0,
 'student_distill_rate' : 1.0,
@@ -441,5 +462,6 @@ DISTILL_PARAMS = {'pure_dstl_1' :  pure_dstl_1,
                   'dstl01_910_crs_slwfst_2': dstl01_910_crs_slwfst_2,
                   'rpdst01_019_crs_slwfst_2': rpdst01_019_crs_slwfst_2,
                   'schdexp_dstl5_910_crs_slwfst_2': schdexp_dstl5_910_crs_slwfst_2,
-                  'schdexp_dstl_910_crs_slwfst_2': schdexp_dstl_910_crs_slwfst_2
+                  'schdexp_dstl_910_crs_slwfst_2': schdexp_dstl_910_crs_slwfst_2,
+                  'schdexp_dstl01_910_crs_slwfst_2': schdexp_dstl01_910_crs_slwfst_2,
                   }
