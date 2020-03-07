eebe94c08e47aecf74042de161914f45a74f976b
Samira Abnar <samiraabnar@gmail.com>
2020-03-07 18:39:36 +0100

    scheduled online

--- tf2_models/metrics.py
+++ tf2_models/metrics.py
@@ -4,9 +4,7 @@ import tensorflow as tf
 @tf.function(experimental_relax_shapes=True)
 def distill_loss(y_true, y_pred, tmp):
   y_true = tf.cast(tf.squeeze(y_true), dtype=tf.float32)
-  tf.print("tmp: ",tmp)
   scale_factor = 1.0 / (tmp*tmp)
-  tf.print("scale_factor", scale_factor)
   return tf.reduce_mean(tf.compat.v2.nn.softmax_cross_entropy_with_logits(logits=y_pred / tmp,
                                                                          labels=y_true,
                                                                          name='loss')) * scale_factor
