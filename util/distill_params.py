5414f2f066302476d784a21ddd0a01c0413e52c5
Samira Abnar <samiraabnar@gmail.com>
2020-03-08 16:37:32 +0100

    new runs

--- util/distill_params.py
+++ util/distill_params.py
@@ -287,6 +287,24 @@ rpdst_019_crs_slwfst_2 = {
 'schedule': 'crs_fst'
 }
 
+rpdst_0010_crs_slwfst_2 = {
+'distill_temp' : 1.0,
+'student_distill_rate' : 0.0,
+'student_gold_rate' : 0.0,
+'student_distill_rep_rate': 1.0,
+'student_learning_rate' : 0.0001,
+'student_decay_steps' : 10000,
+'student_hold_base_rate_steps' :  0,
+'student_warmup_steps' : 0,
+'student_optimizer' : 'adam',
+'teacher_learning_rate' : 0.001,
+'teacher_decay_steps' : 10000,
+'teacher_warmup_steps' : 10000,
+'teacher_hold_base_rate_steps' : 0,
+'teacher_optimizer' : 'radam',
+'schedule': 'crs_fst'
+}
+
 dstl_910_crs_slwfst_2 = {
 'distill_temp' : 1.0,
 'student_distill_rate' : 0.9,
@@ -378,6 +396,42 @@ rpdst5_019_crs_slwfst_3 = {
 'schedule': 'crs_fst'
 }
 
+rpdst_019_crs_slwfst_3 = {
+'distill_temp' : 1.0,
+'student_distill_rate' : 0.0,
+'student_gold_rate' : 0.1,
+'student_distill_rep_rate': 0.9,
+'student_learning_rate' : 0.001,
+'student_decay_steps' : 10000,
+'student_hold_base_rate_steps' :  0,
+'student_warmup_steps' : 0,
+'student_optimizer' : 'adam',
+'teacher_learning_rate' : 0.001,
+'teacher_decay_steps' : 10000,
+'teacher_warmup_steps' : 5000,
+'teacher_hold_base_rate_steps' : 1000000,
+'teacher_optimizer' : 'radam',
+'schedule': 'crs_fst'
+}
+
+rpdst_0010_crs_slwfst_3 = {
+'distill_temp' : 1.0,
+'student_distill_rate' : 0.0,
+'student_gold_rate' : 0.0,
+'student_distill_rep_rate': 1.0,
+'student_learning_rate' : 0.001,
+'student_decay_steps' : 10000,
+'student_hold_base_rate_steps' :  0,
+'student_warmup_steps' : 0,
+'student_optimizer' : 'adam',
+'teacher_learning_rate' : 0.001,
+'teacher_decay_steps' : 10000,
+'teacher_warmup_steps' : 5000,
+'teacher_hold_base_rate_steps' : 1000000,
+'teacher_optimizer' : 'radam',
+'schedule': 'crs_fst'
+}
+
 rpdst01_019_crs_slwfst_2 = {
 'distill_temp' : 0.1,
 'student_distill_rate' : 0.0,
@@ -395,7 +449,6 @@ rpdst01_019_crs_slwfst_2 = {
 'teacher_optimizer' : 'radam',
 'schedule': 'crs_fst'
 }
-
 
 dstl01_910_crs_slwfst_2 = {
 'distill_temp' : 0.1,
@@ -434,7 +487,6 @@ schdexp_dstl01_910_crs_slwfst_2 = {
 'teacher_optimizer' : 'radam',
 'schedule': 'crs_fst'
 }
-
 
 schdexp_dstl_910_crs_slwfst_2 = {
 'distill_temp' : 1.0,
@@ -502,4 +554,7 @@ DISTILL_PARAMS = {'pure_dstl_1' :  pure_dstl_1,
                   'schdexp_dstl01_910_crs_slwfst_2': schdexp_dstl01_910_crs_slwfst_2,
                   'rpdst5_019_crs_slwfst_3': rpdst5_019_crs_slwfst_3,
                   'dstl5_910_crs_slwfst_3': dstl5_910_crs_slwfst_3,
+                  'rpdst_019_crs_slwfst_3': rpdst_019_crs_slwfst_3,
+                  'rpdst_0010_crs_slwfst_2': rpdst_0010_crs_slwfst_2,
+                  'rpdst_0010_crs_slwfst_3': rpdst_0010_crs_slwfst_3
                   }
