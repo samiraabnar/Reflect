7152ba13e62410a39783cd47b7e78eeed8e1059a
Samira Abnar <samiraabnar@gmail.com>
2020-03-09 14:25:56 +0100

    lm1b

--- tfds_data/lm1b.py
+++ tfds_data/lm1b.py
@@ -14,69 +14,77 @@ from util.text_util import deps_from_tsv, deps_to_tsv
 
 def build_dic(data_dir):
   worddict = {}
+  reserved = [constants.pad, constants.unk, constants.bos, constants.eos]
   worddict[constants.pad] = constants.pad_idx
   worddict[constants.unk] = constants.unk_idx
   worddict[constants.bos] = constants.bos_idx
   worddict[constants.eos] = constants.eos_idx
 
+  text_encoder_config = tfds.features.text.TextEncoderConfig(
+    encoder_cls=tfds.features.text.SubwordTextEncoder,
+    vocab_size=2 ** 13)
+
+  feature = tfds.features.Text(
+    encoder_config=text_encoder_config)
+
   databuilder = tfds.text.lm1b.Lm1b(data_dir='data')
   databuilder.download_and_prepare(download_dir='tmp/',
                                    download_config=tfds.download.DownloadConfig(register_checksums=True))
 
-  dataset = databuilder.as_dataset(split="train")
-  dataset = tfds.as_numpy(dataset)
-  for sentence in dataset:
-    words = sentence['text'].split()
-    for word in words:
-      if word not in worddict:
-        worddict[word] = len(worddict)
+  def generator():
+    dataset = databuilder.as_dataset(split="train")
+    dataset = tfds.as_numpy(dataset)
+    for sentence in dataset:
+      yield sentence
 
-  print(len(worddict))
+  feature.encoder.build_from_corpus(
+                    generator,
+                    target_vocab_size=2 ** 13,
+                    max_subword_length=20,
+                    max_corpus_chars=None,
+                    reserved_tokens=reserved)
 
-  vocab_file = os.path.join(data_dir, 'vocab')
-  print('| write vocabulary to %s' % vocab_file)
-
-  np.save(vocab_file, arr=worddict)
+  feature.encoder.save_to_file(filename_prefix=os.path.join(data_dir,"sublmvocab"))
 
 
 if __name__ == '__main__':
   vocab_dir = 'data/lm1b/vocab'
   build_dic(data_dir='data/lm1b')
-
-  vocab = list(np.load(vocab_dir, allow_pickle=True).item().keys())
-  print("Vocab len: ", len(vocab))
-  text_encoder_config = tfds.features.text.TextEncoderConfig(
-    encoder=tfds.features.text.TokenTextEncoder(vocab_list=vocab,
-                                                oov_token=constants.unk,
-                                                lowercase=False, tokenizer=tfds.features.text.Tokenizer(
-        alphanum_only=True,
-        reserved_tokens=[a for a in string.punctuation if a not in ['<', '>']] + constants.all
-      )))
-
-  config = Lm1bConfig(
-    name="bytes",
-    version="0.0.1",
-    description=("Uses token text encoding with "
-                 "`tfds.features.text.ByteTextEncoder`"),
-    text_encoder_config=tfds.features.text.TextEncoderConfig(
-      encoder=tfds.features.text.ByteTextEncoder()),
-  ),
-
-  databuilder = tfds.text.lm1b.Lm1b(data_dir='data',
-                                    config=config)
-  databuilder.download_and_prepare(download_dir='tmp/',
-                                    download_config=tfds.download.DownloadConfig(register_checksums=True))
-
-
-  dataset = databuilder.as_dataset(split="test", batch_size=1000)
-  dataset = tfds.as_numpy(dataset)
-  for batch in dataset:
-    print(batch[0])
-
-    break
-
-  print(databuilder.info.features.keys())
-  print(databuilder.info.features['text'].vocab_size)
-
-
-
+  #
+  # vocab = list(np.load(vocab_dir, allow_pickle=True).item().keys())
+  # print("Vocab len: ", len(vocab))
+  # text_encoder_config = tfds.features.text.TextEncoderConfig(
+  #   encoder=tfds.features.text.TokenTextEncoder(vocab_list=vocab,
+  #                                               oov_token=constants.unk,
+  #                                               lowercase=False, tokenizer=tfds.features.text.Tokenizer(
+  #       alphanum_only=True,
+  #       reserved_tokens=[a for a in string.punctuation if a not in ['<', '>']] + constants.all
+  #     )))
+  #
+  # config = Lm1bConfig(
+  #   name="bytes",
+  #   version="0.0.1",
+  #   description=("Uses token text encoding with "
+  #                "`tfds.features.text.ByteTextEncoder`"),
+  #   text_encoder_config=tfds.features.text.TextEncoderConfig(
+  #     encoder=tfds.features.text.ByteTextEncoder()),
+  # ),
+  #
+  # databuilder = tfds.text.lm1b.Lm1b(data_dir='data',
+  #                                   config=config)
+  # databuilder.download_and_prepare(download_dir='tmp/',
+  #                                   download_config=tfds.download.DownloadConfig(register_checksums=True))
+  #
+  #
+  # dataset = databuilder.as_dataset(split="test", batch_size=1000)
+  # dataset = tfds.as_numpy(dataset)
+  # for batch in dataset:
+  #   print(batch[0])
+  #
+  #   break
+  #
+  # print(databuilder.info.features.keys())
+  # print(databuilder.info.features['text'].vocab_size)
+  # 
+  #
+  #
