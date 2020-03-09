09ac7b4a2d85bf22f1fe352e2efd5860b2c7fa23
Samira Abnar <samiraabnar@gmail.com>
2020-03-09 23:30:12 +0100

    lm1b tfds

--- tfds_data/lm1b.py
+++ tfds_data/lm1b.py
@@ -58,12 +58,11 @@ if __name__ == '__main__':
   )
 
   config = Lm1bConfig(
-    name="bytes",
+    name="subwords",
     version="0.0.1",
     description=("Uses token text encoding with "
-                 "`tfds.features.text.ByteTextEncoder`"),
-    text_encoder_config=tfds.features.text.TextEncoderConfig(
-      encoder=tfds.features.text.ByteTextEncoder()),
+                 "`tfds.features.text.SubwordTextEncodereTextEncoder`"),
+    text_encoder_config=text_encoder_config,
   ),
 
   databuilder = tfds.text.lm1b.Lm1b(data_dir='data',
