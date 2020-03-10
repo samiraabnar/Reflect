0109bdc9462526883d084be67ada8d26945b036e
Samira Abnar <samiraabnar@gmail.com>
2020-03-10 11:31:12 +0100

    lm1b tfds

--- tfds_data/lm1b.py
+++ tfds_data/lm1b.py
@@ -73,7 +73,8 @@ if __name__ == '__main__':
   dataset = tfds.as_numpy(dataset)
   for batch in dataset:
     print(batch['text'][0])
-
+    print(databuilder.info.features["text"].decoder(batch['text'][0]))
+    print(databuilder.info.features["text"].encoder('<bos> hi <eos>'))
     break
   #
   # print(databuilder.info.features.keys())
