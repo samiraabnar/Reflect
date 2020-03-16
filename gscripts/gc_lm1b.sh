8c16171a4e55336b3d4cdf697730c6e2f15ad40b
Samira Abnar <samiraabnar@gmail.com>
2020-03-16 10:19:54 +0100

    change running script

--- gscripts/gc_lm1b.sh
+++ gscripts/gc_lm1b.sh
@@ -7,27 +7,21 @@ cd ~/Codes/InDist
 export PYTHONPATH=$PYTHONPATH:/home/dehghani/Codes/InDist
 
 
-CUDA_VISIBLE_DEVICES=0 python keras_trainer.py \
+CUDA_VISIBLE_DEVICES=0,1,2,3 python keras_trainer.py \
 --model=lm_lstm_shared_emb \
 --task=lm1b \
 --model_config=biglstm_drop31_v2 \
 --train_config=radam_slw \
+--batch_size=128 \
 --exp_name=offlineteacher_v1 > lm1b_run1 &
 
 
-CUDA_VISIBLE_DEVICES=1 python keras_trainer.py \
+CUDA_VISIBLE_DEVICES=4,5,6,7 python keras_trainer.py \
 --model=lm_lstm_shared_emb \
 --task=lm1b \
 --model_config=biglstm_drop31_v2 \
 --train_config=radam_fst \
+--batch_size=128 \
 --exp_name=offlineteacher_v2 > lm1b_run2&
-
-
-CUDA_VISIBLE_DEVICES=2 python keras_trainer.py \
---model=lm_lstm_shared_emb \
---task=lm1b \
---model_config=biglstm_drop31_v2 \
---train_config=crs_slw \
---exp_name=offlineteacher_v3 > lm1b_run3&
 
 wait
