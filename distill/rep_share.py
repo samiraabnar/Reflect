8d78227b7592843b7d61362dcc0749942eb438c7
Samira Abnar <samiraabnar@gmail.com>
2020-03-18 10:39:39 +0100

    fix an error

--- distill/rep_share.py
+++ distill/rep_share.py
@@ -58,7 +58,7 @@ class OnlineRepDistiller(OnlineDistiller):
 
 
   def distill_loop(self):
-    #@tf.function(experimental_relax_shapes=True)
+    @tf.function(experimental_relax_shapes=True)
     def teacher_train_step(x, y_true):
       with tf.GradientTape() as tape:
         teacher_logits, teacher_reps = get_reps(x, self.teacher_model,
@@ -77,7 +77,7 @@ class OnlineRepDistiller(OnlineDistiller):
 
       return teacher_logits, teacher_reps, final_loss
 
-    #@tf.function(experimental_relax_shapes=True)
+    @tf.function(experimental_relax_shapes=True)
     def student_train_step(x, y_s, teacher_probs, teacher_reps):
       ''' Training step for the student model (this is the only training step for offline distillation).
 
@@ -106,7 +106,7 @@ class OnlineRepDistiller(OnlineDistiller):
 
       return rep_loss, actual_loss
 
-    #@tf.function(experimental_relax_shapes=True)
+    @tf.function(experimental_relax_shapes=True)
     def epoch_step_fn(x_s, y_s):
       teacher_logits, teacher_reps, teacher_loss = teacher_train_step(x_s, y_s)
       teacher_probs = self.teacher_task_probs_fn(logits=teacher_logits, labels=y_s, temperature=self.temperature)
@@ -120,7 +120,7 @@ class OnlineRepDistiller(OnlineDistiller):
 
     #@tf.function(experimental_relax_shapes=True)
     def epoch_step(x_s, y_s):
-      teacher_loss, distill_loss, actual_loss = self.strategy.experimental_run_v2(epoch_step_fn, (x_s, y_s))
+      teacher_loss, distill_loss, actual_loss = self.strategy.experimental_run(epoch_step_fn, (x_s, y_s))
 
       return teacher_loss, distill_loss, actual_loss
 
