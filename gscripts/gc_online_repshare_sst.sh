5b40aae838af8c744efffeee938f4ac38a2f135c
Samira Abnar <samiraabnar@gmail.com>
2020-03-23 11:24:21 +0100

    new runs

--- gscripts/gc_online_repshare_sst.sh
+++ gscripts/gc_online_repshare_sst.sh
@@ -2,58 +2,26 @@
 
 CUDA_VISIBLE_DEVICES=0 python distill/repshare_main.py --task=sst2  \
 --teacher_model=cl_lstm \
---student_model=cl_lstm \
---teacher_exp_name=gc_or_tchr1 \
+--student_model=cl_bert \
+--teacher_exp_name=gc_or_dtchr1 \
 --student_exp_name=gc_std1 \
 --teacher_config=small_lstm_v4 \
---student_config=small_lstm_v4 \
+--student_config=small_gpt_v9 \
 --batch_size=128 \
 --distill_mode=rep_online \
---distill_config=rpdst_019_crs_slwfst_sst1 > run_sst1 &
+--distill_config=rpdst_019_crs_slwfst_sst1 > run_sst6 &
 
 CUDA_VISIBLE_DEVICES=1 python distill/repshare_main.py --task=sst2  \
 --teacher_model=cl_lstm \
---student_model=cl_lstm \
---teacher_exp_name=gc_or_tchr2 \
+--student_model=cl_bert \
+--teacher_exp_name=gc_or_dtchr2 \
 --student_exp_name=gc_std2 \
 --teacher_config=small_lstm_v4 \
---student_config=small_lstm_v4 \
+--student_config=small_gpt_v9 \
 --batch_size=128 \
 --distill_mode=rep_online \
---distill_config=rpdst_019_crs_slwfst_sst2 > run_sst2 &
-
-CUDA_VISIBLE_DEVICES=2 python distill/repshare_main.py --task=sst2  \
---teacher_model=cl_lstm \
---student_model=cl_lstm \
---teacher_exp_name=gc_or_tchr3 \
---student_exp_name=gc_std3 \
---teacher_config=small_lstm_v4 \
---student_config=small_lstm_v4 \
---batch_size=128 \
---distill_mode=rep_online \
---distill_config=rpdst_019_crs_slwfst_sst3 > run_sst3 &
-
-CUDA_VISIBLE_DEVICES=3 python distill/repshare_main.py --task=sst2  \
---teacher_model=cl_lstm \
---student_model=cl_lstm \
---teacher_exp_name=gc_or_tchr4 \
---student_exp_name=gc_std4 \
---teacher_config=small_lstm_v4 \
---student_config=small_lstm_v4 \
---batch_size=128 \
---distill_mode=rep_online \
---distill_config=rpdst_019_crs_slwfst_sst4 > run_sst4 &
+--distill_config=rpdst_019_crs_slwfst_sst2 > run_sst7 &
 
 
-CUDA_VISIBLE_DEVICES=4 python distill/repshare_main.py --task=sst2  \
---teacher_model=cl_lstm \
---student_model=cl_lstm \
---teacher_exp_name=gc_or_tchr5 \
---student_exp_name=gc_std5 \
---teacher_config=small_lstm_v4 \
---student_config=small_lstm_v4 \
---batch_size=128 \
---distill_mode=rep_online \
---distill_config=rpdst_019_crs_slwfst_sst5  > run_sst5 &
 
 wait
