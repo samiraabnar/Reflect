b0cb180dd570f49a33746839a9dd38304ba5f08a
Samira Abnar <samiraabnar@gmail.com>
2020-03-23 11:29:33 +0100

    new runs

--- gscripts/gc_online_repshare_sst.sh
+++ gscripts/gc_online_repshare_sst.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-CUDA_VISIBLE_DEVICES=0 python distill/repshare_main.py --task=sst2  \
+CUDA_VISIBLE_DEVICES=5 python distill/repshare_main.py --task=sst2  \
 --teacher_model=cl_lstm \
 --student_model=cl_bert \
 --teacher_exp_name=gc_or_dtchr1 \
@@ -11,7 +11,7 @@ CUDA_VISIBLE_DEVICES=0 python distill/repshare_main.py --task=sst2  \
 --distill_mode=rep_online \
 --distill_config=rpdst_019_crs_slwfst_sst1 > run_sst6 &
 
-CUDA_VISIBLE_DEVICES=1 python distill/repshare_main.py --task=sst2  \
+CUDA_VISIBLE_DEVICES=6 python distill/repshare_main.py --task=sst2  \
 --teacher_model=cl_lstm \
 --student_model=cl_bert \
 --teacher_exp_name=gc_or_dtchr2 \
