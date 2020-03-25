0b441510e9d8246b1ab654cc03f49cb6e9c5e8f0
Samira Abnar <samiraabnar@gmail.com>
2020-03-25 10:43:03 +0100

    new run

--- gscripts/gc_online_repshare_lm_sv.sh
+++ gscripts/gc_online_repshare_lm_sv.sh
@@ -1,6 +1,17 @@
 #!/usr/bin/env bash
 
-CUDA_VISIBLE_DEVICES=3 python distill/repshare_main.py --task=word_sv_agreement_lm  \
+CUDA_VISIBLE_DEVICES=0 python distill/repshare_main.py --task=word_sv_agreement_lm  \
+--teacher_model=lm_lstm_shared_emb \
+--student_model=lm_lstm_shared_emb \
+--teacher_exp_name=gc_or_tchr1 \
+--student_exp_name=gc_std1 \
+--teacher_config=lstm_drop31_v2 \
+--student_config=lstm_drop31_v2 \
+--batch_size=64 \
+--distill_mode=rep_online \
+--distill_config=rpdst_019_crs_slwfst_2 > run_sv_lm1 &
+
+CUDA_VISIBLE_DEVICES=1 python distill/repshare_main.py --task=word_sv_agreement_lm  \
 --teacher_model=lm_lstm_shared_emb \
 --student_model=lm_lstm_shared_emb \
 --teacher_exp_name=gc_or_tchr2 \
@@ -9,17 +20,28 @@ CUDA_VISIBLE_DEVICES=3 python distill/repshare_main.py --task=word_sv_agreement_
 --student_config=lstm_drop31_v2 \
 --batch_size=64 \
 --distill_mode=rep_online \
---distill_config=rpdst_019_crs_slwfst_3 > run_sv_lm4 &
+--distill_config=rpdst_019_crs_slwfst_3 > run_sv_lm2 &
 
-CUDA_VISIBLE_DEVICES=4 python distill/repshare_main.py --task=word_sv_agreement_lm  \
+CUDA_VISIBLE_DEVICES=2 python distill/repshare_main.py --task=word_sv_agreement_lm  \
 --teacher_model=lm_lstm_shared_emb \
 --student_model=lm_gpt2_shared \
---teacher_exp_name=gc_or_dtchr3 \
---student_exp_name=gc_std3 \
---teacher_config=very_big_gpt_v10 \
---student_config=lstm_drop31_v2 \
+--teacher_exp_name=gc_or_dtchr1 \
+--student_exp_name=gc_std1 \
+--teacher_config=lstm_drop31_v2 \
+--student_config=very_big_gpt_v10 \
 --batch_size=64 \
 --distill_mode=rep_online \
---distill_config=rpdst_019_crs_slwfst > run_sv_lm5 &
+--distill_config=rpdst_019_crs_slwfst_2 > run_sv_lm3 &
+
+CUDA_VISIBLE_DEVICES=3 python distill/repshare_main.py --task=word_sv_agreement_lm  \
+--teacher_model=lm_lstm_shared_emb \
+--student_model=lm_gpt2_shared \
+--teacher_exp_name=gc_or_dtchr2 \
+--student_exp_name=gc_std2 \
+--teacher_config=lstm_drop31_v2 \
+--student_config=very_big_gpt_v10 \
+--batch_size=64 \
+--distill_mode=rep_online \
+--distill_config=rpdst_019_crs_slwfst_3 > run_sv_lm4 &
 
 wait
