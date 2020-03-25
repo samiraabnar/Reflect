1740aa950a9cd414f9660393e5f38bf19742e9dc
Samira Abnar <samiraabnar@gmail.com>
2020-03-25 21:16:54 +0100

    new config, new run

--- gscripts/gc_online_repshare_sst.sh
+++ gscripts/gc_online_repshare_sst.sh
@@ -6,45 +6,45 @@ export PYTHONPATH=$PYTHONPATH:~/Codes/InDist
 CUDA_VISIBLE_DEVICES=4 python distill/repshare_main.py --task=sst2  \
 --teacher_model=cl_lstm \
 --student_model=cl_lstm \
---teacher_exp_name=gc_or_tchr1 \
---student_exp_name=gc_std1 \
+--teacher_exp_name=gc_or_tchr5 \
+--student_exp_name=gc_std5 \
 --teacher_config=small_lstm_v4 \
 --student_config=small_lstm_v4 \
 --batch_size=128 \
 --distill_mode=rep_online \
---distill_config=rpdst_019_crs_slwfst_sst1 > run_sst1 &
+--distill_config=rpdst_019_crs_slwfst_sst6 > run_sst5 &
 
 CUDA_VISIBLE_DEVICES=5 python distill/repshare_main.py --task=sst2  \
 --teacher_model=cl_lstm \
 --student_model=cl_lstm \
---teacher_exp_name=gc_or_tchr2 \
---student_exp_name=gc_std2 \
+--teacher_exp_name=gc_or_tchr6 \
+--student_exp_name=gc_std6 \
 --teacher_config=small_lstm_v4 \
 --student_config=small_lstm_v4 \
 --batch_size=128 \
 --distill_mode=rep_online \
---distill_config=rpdst_019_crs_slwfst_sst2 > run_sst2 &
+--distill_config=rpdst_00199_crs_slwfst_sst6 > run_sst6 &
 
 CUDA_VISIBLE_DEVICES=6 python distill/repshare_main.py --task=sst2  \
 --teacher_model=cl_lstm \
 --student_model=cl_lstm \
---teacher_exp_name=gc_or_tchr3 \
---student_exp_name=gc_std3 \
+--teacher_exp_name=gc_or_tchr7 \
+--student_exp_name=gc_std7 \
 --teacher_config=small_lstm_v4 \
 --student_config=small_lstm_v4 \
 --batch_size=128 \
 --distill_mode=rep_online \
---distill_config=rpdst_019_crs_slwfst_sst3 > run_sst3 &
+--distill_config=rpdst_019_crs_slwfst_sst5 > run_sst7 &
 
 CUDA_VISIBLE_DEVICES=7 python distill/repshare_main.py --task=sst2  \
 --teacher_model=cl_lstm \
---student_model=cl_lstm \
---teacher_exp_name=gc_or_tchr4 \
---student_exp_name=gc_std4 \
+--student_model=cl_bert \
+--teacher_exp_name=gc_or_dtchr1 \
+--student_exp_name=gc_std1 \
 --teacher_config=small_lstm_v4 \
---student_config=small_lstm_v4 \
+--student_config=small_gpt_v9 \
 --batch_size=128 \
 --distill_mode=rep_online \
---distill_config=rpdst_019_crs_slwfst_sst4 > run_sst4 &
+--distill_config=rpdst_019_crs_slwfst_sst1 > run_sst8 &
 
 wait
--- util/distill_params.py
+++ util/distill_params.py
@@ -532,6 +532,7 @@ rpdst_019_crs_slwfst_2 = {
 'schedule': 'crs_fst'
 }
 
+
 rpdst_019_crs_slwfst_3 = {
 'distill_temp' : 1.0,
 'student_distill_rate' : 0.0,
@@ -545,6 +546,24 @@ rpdst_019_crs_slwfst_3 = {
 'teacher_learning_rate' : 0.001,
 'teacher_decay_steps' : 10000,
 'teacher_warmup_steps' : 0,
+'teacher_hold_base_rate_steps' : 0,
+'teacher_optimizer' : 'radam',
+'schedule': 'crs_fst'
+}
+
+rpdst_019_crs_slwfst_4 = {
+'distill_temp' : 1.0,
+'student_distill_rate' : 0.0,
+'student_gold_rate' : 0.1,
+'student_distill_rep_rate': 0.9,
+'student_learning_rate' : 0.0001,
+'student_decay_steps' : 100000,
+'student_hold_base_rate_steps' :  0,
+'student_warmup_steps' : 0,
+'student_optimizer' : 'adam',
+'teacher_learning_rate' : 0.001,
+'teacher_decay_steps' : 10000,
+'teacher_warmup_steps' : 0.0,
 'teacher_hold_base_rate_steps' : 0,
 'teacher_optimizer' : 'radam',
 'schedule': 'crs_fst'
@@ -658,6 +677,42 @@ rpdst_019_crs_slwfst_sst5 = {
 'schedule': 'crs_fst',
 }
 
+rpdst_019_crs_slwfst_sst6 = {
+'distill_temp' : 1.0,
+'student_distill_rate' : 0.0,
+'student_gold_rate' : 0.1,
+'student_distill_rep_rate': 0.9,
+'student_learning_rate' : 0.0001,
+'student_decay_steps' : 10000,
+'student_hold_base_rate_steps' :  0,
+'student_warmup_steps' : 0,
+'student_optimizer' : 'adam',
+'teacher_learning_rate' : 0.0001,
+'teacher_decay_steps' : 500,
+'teacher_warmup_steps' : 000,
+'teacher_hold_base_rate_steps' : 5000,
+'teacher_optimizer' : 'radam',
+'schedule': 'crs_fst',
+}
+
+rpdst_00199_crs_slwfst_sst6 = {
+'distill_temp' : 1.0,
+'student_distill_rate' : 0.0,
+'student_gold_rate' : 0.01,
+'student_distill_rep_rate': 0.99,
+'student_learning_rate' : 0.0001,
+'student_decay_steps' : 10000,
+'student_hold_base_rate_steps' :  0,
+'student_warmup_steps' : 0,
+'student_optimizer' : 'adam',
+'teacher_learning_rate' : 0.0001,
+'teacher_decay_steps' : 500,
+'teacher_warmup_steps' : 000,
+'teacher_hold_base_rate_steps' : 5000,
+'teacher_optimizer' : 'radam',
+'schedule': 'crs_fst',
+}
+
 DISTILL_PARAMS = {'pure_dstl_1' :  pure_dstl_1,
                   'pure_dstl_2' :  pure_dstl_2,
                   'pure_dstl_3' :  pure_dstl_3,
@@ -693,5 +748,9 @@ DISTILL_PARAMS = {'pure_dstl_1' :  pure_dstl_1,
                   'rpdst_019_crs_slwfst_sst4': rpdst_019_crs_slwfst_sst4,
                   'rpdst_019_crs_slwfst_sst5': rpdst_019_crs_slwfst_sst5,
                   'rpdst_019_crs_slwslw_2_trns': rpdst_019_crs_slwslw_2_trns,
-                  'rpdst_019_crs_slwslw_3_trns': rpdst_019_crs_slwslw_3_trns
+                  'rpdst_019_crs_slwslw_3_trns': rpdst_019_crs_slwslw_3_trns,
+                  'rpdst_019_crs_slwfst_4': rpdst_019_crs_slwfst_4,
+                  'rpdst_019_crs_slwfst_sst6': rpdst_019_crs_slwfst_sst6,
+                  'rpdst_00199_crs_slwfst_sst6': rpdst_00199_crs_slwfst_sst6
+
                   }
