9961aa76030343100b0a5a116cd58e08ee13dad2
Samira Abnar <samiraabnar@gmail.com>
2020-03-28 13:41:04 +0100

    random runs

--- gscripts/gc_online_mrepshare_lmvp_sv.sh
+++ gscripts/gc_online_mrepshare_lmvp_sv.sh
@@ -80,5 +80,30 @@ CUDA_VISIBLE_DEVICES=5 python distill/repshare_main.py \
 --distill_mode=mrep_online \
 --distill_config=rpdst_off_019_crs_slwfst_2 > run_sv_lmvp10 &
 
+CUDA_VISIBLE_DEVICES=4 python distill/repshare_main.py \
+--teacher_task=word_sv_agreement_lm \
+--student_task=word_sv_agreement_vp  \
+--teacher_model=lm_lstm_shared_emb \
+--student_model=cl_bert_shared \
+--teacher_exp_name=gc_mor_dtchr6 \
+--student_exp_name=gc_std6 \
+--teacher_config=lstm_drop31_v2 \
+--student_config=big_gpt_v5 \
+--batch_size=64 \
+--distill_mode=mrep_online \
+--distill_config=rpdst_019_crs_slwfst_3 > run_sv_lmvp11 &
+
+CUDA_VISIBLE_DEVICES=5 python distill/repshare_main.py \
+--teacher_task=word_sv_agreement_lm \
+--student_task=word_sv_agreement_vp  \
+--teacher_model=lm_lstm_shared_emb \
+--student_model=cl_bert_shared \
+--teacher_exp_name=gc_mor_dtchr7 \
+--student_exp_name=gc_std7 \
+--teacher_config=lstm_drop31_v2 \
+--student_config=big_gpt_v5 \
+--batch_size=64 \
+--distill_mode=mrep_online \
+--distill_config=rpdst_019_crs_slwfst_2 > run_sv_lmvp12 &
 
 wait
