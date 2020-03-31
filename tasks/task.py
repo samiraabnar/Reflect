23e39348e1bcb166d86ae4988746fd0f590a37bc
Samira Abnar <samiraabnar@gmail.com>
2020-03-31 12:28:08 +0200

    changes to tasks

--- tasks/task.py
+++ tasks/task.py
@@ -7,7 +7,6 @@ from util import constants
 class Task(object):
   def __init__(self, task_params, num_replicas_in_sync=1, builder_cls=None, name='abstract_task', data_dir='data'):
     self.name = name
-    self.output_padding_symbol = 0
     self.task_params = task_params
     self.data_dir = data_dir
     self.builder_cls = builder_cls
@@ -16,7 +15,6 @@ class Task(object):
       self.databuilder = self.builder_cls(data_dir=self.data_dir)
 
     self.input_padding_symbol = tf.cast(self.sentence_encoder().encode(constants.pad)[0], dtype=tf.int64)
-    self.output_padding_symbol = None
     self.setup_datasets()
 
 
