ef256b63b2b23a93d9b70cd7726231c40566025f
Samira Abnar <samiraabnar@gmail.com>
2020-04-03 11:21:02 +0200

    new job

--- gscripts/gc_lm1b.sh
+++ gscripts/gc_lm1b.sh
@@ -7,13 +7,13 @@ cd ~/Codes/InDist
 export PYTHONPATH=$PYTHONPATH:/home/dehghani/Codes/InDist
 
 
-CUDA_VISIBLE_DEVICES=2,3,4,5 python keras_trainer.py \
+CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 python keras_trainer.py \
 --model=lm_lstm_shared_emb \
 --task=lm1b \
 --model_config=lstm_drop31_v3 \
 --train_config=radam_slw2 \
---batch_size=256 \
---exp_name=offlineteacher_v3 > lm1b_run3 &
+--batch_size=1024 \
+--exp_name=offlineteacher_v1 > lm1b_run1 &
 
 
 wait
