0c950c4c06cf5155594746d7e791a4756903e4e9
Samira Abnar <samiraabnar@gmail.com>
2020-04-06 13:37:04 +0200

    new lm rep share run, some config changes

--- util/distill_params.py
+++ util/distill_params.py
@@ -959,6 +959,24 @@ rpdst_019_exp_sst10 = {
 'schedule': 'exp',
 }
 
+rpdst_019_exp_sst11 = {
+'distill_temp' : 1.0,
+'student_distill_rate' : 0.0,
+'student_gold_rate' : 0.1,
+'student_distill_rep_rate': 0.9,
+'student_learning_rate' : 0.0001,
+'student_decay_steps' : 100,
+'student_hold_base_rate_steps' :  5000,
+'student_warmup_steps' : 0,
+'student_optimizer' : 'radam',
+'teacher_learning_rate' : 0.0001,
+'teacher_decay_steps' : 100,
+'teacher_warmup_steps' : 0,
+'teacher_hold_base_rate_steps' : 5000,
+'teacher_optimizer' : 'radam',
+'schedule': 'exp',
+}
+
 
 DISTILL_PARAMS = {'pure_dstl_1' :  pure_dstl_1,
                   'pure_dstl_2' :  pure_dstl_2,
@@ -1012,5 +1030,6 @@ DISTILL_PARAMS = {'pure_dstl_1' :  pure_dstl_1,
                   'rpdst_00199_crs_slwfst_sst6_2': rpdst_00199_crs_slwfst_sst6_2,
                   'rpdst_00199_crs_slwfst_sst8_2': rpdst_00199_crs_slwfst_sst8_2,
                   'rpdst_019_crs_slwfst_sst6_2': rpdst_019_crs_slwfst_sst6_2,
-                  'rpdst_019_exp_sst10': rpdst_019_exp_sst10
+                  'rpdst_019_exp_sst10': rpdst_019_exp_sst10,
+                  'rpdst_019_exp_sst11': rpdst_019_exp_sst11
                   }
