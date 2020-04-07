d6eafa8fcb66161bbe26c7b2bfb41e93aea18962
Samira Abnar <samiraabnar@gmail.com>
2020-04-07 23:24:13 +0200

    working on matrix caps

--- tf2_models/caps_layers.py
+++ tf2_models/caps_layers.py
@@ -15,9 +15,7 @@ class ConvCaps(tf.keras.layers.Layer):
     self.weights_regularizer = tf.keras.regularizers.l2(self.hparams.l2)
     self.w =  tf.Variable('w', shape=[1, self.kh_kw_i, self.num_output_caps, 4, 4],
                             dtype=tf.float32,
-                            initializer=tf.random.truncated_normal(
-                            mean=0.0,
-                            stddev=1.0),  # 1.0
+                            initializer=tf.keras.initializers.glorot_normal(),  # 1.0
                             regularizer=self.weights_regularizer)
     self.em_routing = EmRouting(hparams, num_output_caps=num_output_caps)
 
