72382dfd574d26d7429b76f479aa64d08980bb42
Samira Abnar <samiraabnar@gmail.com>
2020-04-08 15:35:45 +0200

    fixing errors of matrix caps

--- tf2_models/caps_util.py
+++ tf2_models/caps_util.py
@@ -22,16 +22,15 @@ def create_routing_map(child_space, k, s):
       (7*7, 5*5)
   """
   parent_space = int((child_space - k) / s + 1)
-  binmap = tf.zeros((child_space ** 2, parent_space ** 2))
+  binmap = tf.Variable(tf.zeros((child_space ** 2, parent_space ** 2)))
   for r in np.arange(parent_space):
     for c in np.arange(parent_space):
       p_idx = r * parent_space + c
       for i in np.arange(k):
         # c_idx stand for child_index; p_idx is parent_index
         c_idx = r * s * child_space + c * s + child_space * i
-        #binmap[(c_idx):(c_idx + k), p_idx] = binmap[(c_idx):(c_idx + k), p_idx] + 1
-        binmap[(c_idx):(c_idx + k), p_idx] += 1
-        
+        binmap[(c_idx):(c_idx + k), p_idx] = binmap[(c_idx):(c_idx + k), p_idx] + 1
+
   return binmap
 
 def kernel_tile(input, kernel, stride):
