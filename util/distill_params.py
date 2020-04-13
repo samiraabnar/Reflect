4f2b9c6316a989f2ab70b549b7441afca86d1826
Samira Abnar <samiraabnar@gmail.com>
2020-04-13 22:27:16 +0200

    new run

--- util/distill_params.py
+++ util/distill_params.py
@@ -1099,6 +1099,27 @@ rpdst_019_exp_sst14 = {
 'teacher_optimizer' : 'radam',
 'schedule': 'exp',
 }
+
+rpdst_019_exp_sst15 = {
+'distill_temp' : 1.0,
+'student_distill_rate' : 0.0,
+'student_gold_rate' : 0.1,
+'student_distill_rep_rate': 0.9,
+'student_learning_rate' : 0.00005,
+'student_decay_steps' : 10000,
+'student_decay_rate': 0.3,
+'student_hold_base_rate_steps' :  10000,
+'student_warmup_steps' : 10000,
+'student_optimizer' : 'adam',
+'teacher_learning_rate' : 0.00005,
+'teacher_decay_steps' : 10000,
+'teacher_decay_rate': 0.5,
+'teacher_warmup_steps' : 0,
+'teacher_hold_base_rate_steps' : 10000,
+'teacher_optimizer' : 'radam',
+'schedule': 'crs_slw',
+}
+
 DISTILL_PARAMS = {'pure_dstl_1' :  pure_dstl_1,
                   'pure_dstl_2' :  pure_dstl_2,
                   'pure_dstl_3' :  pure_dstl_3,
@@ -1159,5 +1180,6 @@ DISTILL_PARAMS = {'pure_dstl_1' :  pure_dstl_1,
                   'rpdst_019_exp_sst13': rpdst_019_exp_sst13,
                   'rpdst_019_crs_slwfst_6': rpdst_019_crs_slwfst_6,
                   'rpdst_019_crs_slwfst_7': rpdst_019_crs_slwfst_7,
-                  'rpdst_019_exp_sst14': rpdst_019_exp_sst14
+                  'rpdst_019_exp_sst14': rpdst_019_exp_sst14,
+                  'rpdst_019_exp_sst15': rpdst_019_exp_sst15
                   }
