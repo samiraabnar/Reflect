2d017fd7f28580e9bc93b301ed26f1ab98d6070b
Samira Abnar <samiraabnar@gmail.com>
2020-04-13 17:14:14 +0200

    new gc runs

--- gscripts/gc_svhn.sh
+++ gscripts/gc_svhn.sh
@@ -8,68 +8,68 @@ export PYTHONPATH=$PYTHONPATH:/home/dehghani/Codes/InDist
 
 
 CUDA_VISIBLE_DEVICES=0 python mnist_trainer.py \
---model=cl_vcnn \
+--model=resnet \
 --task=svhn \
---model_config=vcnn_svhn1 \
+--model_config=rsnt_svhn1 \
 --train_config=svhn_adam_mid \
 --batch_size=128 \
---exp_name=v33 > run1 &
+--exp_name=v1 > run1 &
 
 
 CUDA_VISIBLE_DEVICES=1 python mnist_trainer.py \
---model=cl_vcnn \
+--model=resnet \
 --task=svhn \
---model_config=vcnn_svhn1 \
+--model_config=rsnt_svhn1 \
 --train_config=svhn_radam_mid \
 --batch_size=128 \
---exp_name=v34 > run2 &
+--exp_name=v2 > run2 &
 
 CUDA_VISIBLE_DEVICES=2 python mnist_trainer.py \
---model=cl_vcnn \
+--model=resnet \
 --task=svhn \
---model_config=vcnn_svhn1 \
+--model_config=rsnt_svhn1 \
 --train_config=svhn_crs_slw \
 --batch_size=128 \
---exp_name=v35 > run3 &
+--exp_name=v3 > run3 &
 
 CUDA_VISIBLE_DEVICES=3 python mnist_trainer.py \
---model=cl_vcnn \
+--model=resnet \
 --task=svhn \
---model_config=vcnn_svhn5 \
+--model_config=rsnt_svhn1 \
 --train_config=svhn_crs_slw \
 --batch_size=128 \
---exp_name=v36 > run4 &
+--exp_name=v4 > run4 &
 
 CUDA_VISIBLE_DEVICES=4 python mnist_trainer.py \
---model=cl_vcnn \
+--model=resnet \
 --task=svhn \
---model_config=vcnn_svhn5 \
+--model_config=rsnt_svhn1 \
 --train_config=svhn_radam_mid \
 --batch_size=128 \
---exp_name=v37 > run5 &
+--exp_name=v5 > run5 &
 
 CUDA_VISIBLE_DEVICES=5 python mnist_trainer.py \
---model=cl_vcnn \
+--model=resnet \
 --task=svhn \
---model_config=vcnn_svhn5 \
+--model_config=rsnt_svhn1 \
 --train_config=svhn_adam_mid \
 --batch_size=128 \
---exp_name=v38 > run6 &
+--exp_name=v6 > run6 &
 
 CUDA_VISIBLE_DEVICES=6 python mnist_trainer.py \
---model=cl_vcnn \
+--model=resnet \
 --task=svhn \
---model_config=vcnn_svhn5 \
+--model_config=rsnt_svhn1 \
 --train_config=adam_mid \
 --batch_size=128 \
---exp_name=v39 > run7 &
+--exp_name=v7 > run7 &
 
 CUDA_VISIBLE_DEVICES=7 python mnist_trainer.py \
---model=cl_vcnn \
+--model=resnet \
 --task=svhn \
---model_config=vcnn_svhn5 \
+--model_config=rsnt_svhn1 \
 --train_config=radam_mid \
 --batch_size=128 \
---exp_name=v40 > run8 &
+--exp_name=v8 > run8 &
 
 wait
