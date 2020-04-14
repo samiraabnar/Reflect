4d3441c65ac6791f6a197858773dd8cd20239dd4
Samira Abnar <samiraabnar@gmail.com>
2020-04-14 13:05:19 +0200

    new run

--- util/distill_params.py
+++ util/distill_params.py
@@ -11,7 +11,8 @@ pure_dstl_1 = {
 'teacher_decay_steps' : 10000,
 'teacher_warmup_steps' : 10000,
 'teacher_hold_base_rate_steps' : 1000,
-'teacher_optimizer' : 'adam'
+'teacher_optimizer' : 'adam',
+'n_epochs': 100
 }
 
 
@@ -28,7 +29,8 @@ pure_dstl_2 = {
 'teacher_decay_steps' : 10000,
 'teacher_warmup_steps' : 0,
 'teacher_hold_base_rate_steps' : 0,
-'teacher_optimizer' : 'radam'
+'teacher_optimizer' : 'radam',
+'n_epochs': 100
 }
 
 pure_dstl_3 = {
@@ -44,23 +46,43 @@ pure_dstl_3 = {
 'teacher_decay_steps' : 10000,
 'teacher_warmup_steps' : 0,
 'teacher_hold_base_rate_steps' : 0,
-'teacher_optimizer' : 'radam'
+'teacher_optimizer' : 'radam',
+'n_epochs': 100,
+
 }
 
 pure_dstl_4 = {
-'distill_temp' : 1.0,
+'distill_temp' : 5.0,
 'student_distill_rate' : 1.0,
 'student_gold_rate' : 0.0,
-'student_learning_rate' : 0.0001,
+'student_learning_rate' : 0.00005,
 'student_decay_steps' : 10000,
 'student_hold_base_rate_steps' :  1000,
 'student_warmup_steps' : 10000,
 'student_optimizer' : 'adam',
-'teacher_learning_rate' : 0.0001,
+'teacher_learning_rate' : 0.00005,
 'teacher_decay_steps' : 10000,
 'teacher_warmup_steps' : 0,
 'teacher_hold_base_rate_steps' : 0,
-'teacher_optimizer' : 'radam'
+'teacher_optimizer' : 'radam',
+'n_epochs': 100
+}
+
+pure_dstl_6 = {
+'distill_temp' : 5.0,
+'student_distill_rate' : 1.0,
+'student_gold_rate' : 0.0,
+'student_learning_rate' : 0.00005,
+'student_decay_steps' : 10000,
+'student_hold_base_rate_steps' :  10000,
+'student_warmup_steps' : 10000,
+'student_optimizer' : 'adam',
+'teacher_learning_rate' : 0.00005,
+'teacher_decay_steps' : 10000,
+'teacher_warmup_steps' : 0,
+'teacher_hold_base_rate_steps' : 0,
+'teacher_optimizer' : 'radam',
+'n_epochs': 100
 }
 
 pure_dstl_4_radamfst = {
@@ -111,6 +133,24 @@ pure_dstl5_4_crs_fst = {
 'teacher_hold_base_rate_steps' : 0,
 'teacher_optimizer' : 'radam',
 'schedule': 'crs'
+}
+
+pure_dstl5_4_crs_fst = {
+'distill_temp' : 5.0,
+'student_distill_rate' : 1.0,
+'student_gold_rate' : 0.0,
+'student_learning_rate' : 0.0001,
+'student_decay_steps' : 1000,
+'student_hold_base_rate_steps' :  0,
+'student_warmup_steps' : 0,
+'student_optimizer' : 'adam',
+'teacher_learning_rate' : 0.0001,
+'teacher_decay_steps' : 10000,
+'teacher_warmup_steps' : 0,
+'teacher_hold_base_rate_steps' : 0,
+'teacher_optimizer' : 'radam',
+'schedule': 'crs',
+'n_epochs': 100,
 }
 pure_dstl_4_crs_slw = {
 'distill_temp' : 1.0,
@@ -1195,6 +1235,7 @@ DISTILL_PARAMS = {'pure_dstl_1' :  pure_dstl_1,
                   'pure_dstl_2' :  pure_dstl_2,
                   'pure_dstl_3' :  pure_dstl_3,
                   'pure_dstl_4': pure_dstl_4,
+                  'pure_dstl_6': pure_dstl_6,
                   'pure_dstl_4_radamfst': pure_dstl_4_radamfst,
                   'pure_dstl_5': pure_dstl_5,
                   'pure_dstl_4_fstonln': pure_dstl_4_fstonln,
