093de6b620d953ed97a3f63c200915e327958c5e
Mostafa Dehghani <dehghani.mostafa@gmail.com>
2020-04-15 13:51:46 +0200

    fixing ff

--- tf2_models/resnet.py
+++ tf2_models/resnet.py
@@ -60,18 +60,17 @@ class Resnet(tf.keras.Model):
 
   def create_layers(self):
     self.batch_norm1 = tf.keras.layers.BatchNormalization()
-    self.batch_norm2 = tf.keras.layers.BatchNormalization()
-    self.batch_norm3 = tf.keras.layers.BatchNormalization()
-    self.batch_norm4 = tf.keras.layers.BatchNormalization()
     self.activation = tf.keras.layers.Activation('relu')
 
 
     self.conv1 = tf.keras.layers.Conv2D(self.hparams.filters[0], self.hparams.kernel_size[0],
                                   activation=None,
                                   kernel_regularizer=self.regularizer)
+    self.batch_norm2 = tf.keras.layers.BatchNormalization()
     self.conv2 = tf.keras.layers.Conv2D(self.hparams.filters[1], self.hparams.kernel_size[1],
                                   activation=None,
                                   kernel_regularizer=self.regularizer)
+    self.batch_norm3 = tf.keras.layers.BatchNormalization()
     self.pool2 = tf.keras.layers.MaxPooling2D(self.hparams.pool_size)
 
     self.resblocks = []
@@ -80,6 +79,7 @@ class Resnet(tf.keras.Model):
 
     self.conv4 = tf.keras.layers.Conv2D(self.hparams.filters[3], self.hparams.kernel_size[3],
                                         activation=None)
+    self.batch_norm4 = tf.keras.layers.BatchNormalization()
     self.avgpool = tf.keras.layers.GlobalAveragePooling2D()
     self.dense = tf.keras.layers.Dense(self.hparams.hidden_dim, activation='relu')
     self.dropout = tf.keras.layers.Dropout(self.hparams.hidden_dropout_rate)
@@ -124,6 +124,7 @@ class Resnet(tf.keras.Model):
     x = self.batch_norm2(x, training=training, **kwargs)
     x = self.activation(x)
     x = self.dropout(x, training=training, **kwargs)
+
     self.layer_activations.append(x)
 
     x = self.conv2(x, training=training, **kwargs)
