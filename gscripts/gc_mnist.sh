17f6ab427f812193c56cbe88d058d6a1ef95ce7c
Mostafa Dehghani <dehghani.mostafa@gmail.com>
2020-04-20 13:31:26 +0200

    vanilaa cnn runs

--- gscripts/gc_mnist.sh
+++ gscripts/gc_mnist.sh
@@ -220,37 +220,37 @@ export PYTHONPATH=$PYTHONPATH:/home/dehghani/Codes/InDist
 #--distill_config=pure_dstl5_4_crs_slw_3 > o_run1 &
 
 
+#CUDA_VISIBLE_DEVICES=0 python distill/distill_main.py \
+#--task=mnist \
+#--teacher_model=cl_vcnn \
+#--student_model=cl_vcnn \
+#--student_exp_name=gc_f_std17 \
+#--teacher_exp_name=gc_o_tchr17 \
+#--teacher_config=vcnn_mnist7 \
+#--student_config=vcnn_mnist7 \
+#--distill_mode=offline \
+#--batch_size=128 \
+#--keep_some_checkpoints=True \
+#--max_checkpoints=15 \
+#--distill_config=pure_dstl5_4_crs_slw_3 > o_run6 &
+
+
 CUDA_VISIBLE_DEVICES=0 python distill/distill_main.py \
 --task=mnist \
 --teacher_model=cl_vcnn \
 --student_model=cl_vcnn \
---student_exp_name=gc_f_std17 \
---teacher_exp_name=gc_o_tchr17 \
+--student_exp_name=gc_f_std18 \
+--teacher_exp_name=gc_o_tchr18 \
 --teacher_config=vcnn_mnist7 \
 --student_config=vcnn_mnist7 \
 --distill_mode=offline \
 --batch_size=128 \
 --keep_some_checkpoints=True \
 --max_checkpoints=15 \
---distill_config=pure_dstl5_4_crs_slw_3 > o_run6 &
+--distill_config=pure_dstl5_4_crs_slw_3 > o_run1 &
 
 
 CUDA_VISIBLE_DEVICES=1 python distill/distill_main.py \
---task=mnist \
---teacher_model=cl_vcnn \
---student_model=cl_vcnn \
---student_exp_name=gc_o_std18 \
---teacher_exp_name=gc_o_tchr18 \
---teacher_config=vcnn_mnist7 \
---student_config=vcnn_mnist7 \
---distill_mode=online \
---batch_size=128 \
---keep_some_checkpoints=True \
---max_checkpoints=15 \
---distill_config=pure_dstl2_4_crs_slw_3 > o_run1 &
-
-
-CUDA_VISIBLE_DEVICES=2 python distill/distill_main.py \
 --task=mnist \
 --teacher_model=cl_vcnn \
 --student_model=cl_vcnn \
@@ -260,11 +260,11 @@ CUDA_VISIBLE_DEVICES=2 python distill/distill_main.py \
 --student_config=vcnn_mnist7 \
 --distill_mode=online \
 --batch_size=128 \
-#--keep_some_checkpoints=True \
-#--max_checkpoints=15 \
+--keep_some_checkpoints=True \
+--max_checkpoints=15 \
 --distill_config=pure_dstl2_4_crs_slw_3 > o_run1 &
 
-CUDA_VISIBLE_DEVICES=3 python distill/distill_main.py \
+CUDA_VISIBLE_DEVICES=2 python distill/distill_main.py \
 --task=mnist \
 --teacher_model=cl_vcnn \
 --student_model=cl_vcnn \
@@ -278,26 +278,41 @@ CUDA_VISIBLE_DEVICES=3 python distill/distill_main.py \
 #--max_checkpoints=15 \
 --distill_config=pure_dstl2_4_crs_slw_3 > o_run1 &
 
+CUDA_VISIBLE_DEVICES=3 python distill/distill_main.py \
+--task=mnist \
+--teacher_model=cl_vff \
+--student_model=cl_vcnn \
+--student_exp_name=gc_f_std100 \
+--teacher_exp_name=gc_o_dtchr100 \
+--teacher_config=ff_mnist4 \
+--student_config=vcnn_mnist7 \
+--distill_mode=offline \
+--batch_size=128 \
+--keep_some_checkpoints=True \
+--max_checkpoints=15 \
+--distill_config=pure_dstl5_4_crs_slw_3 > o_run1 &
+
 CUDA_VISIBLE_DEVICES=4 python distill/distill_main.py \
 --task=mnist \
 --teacher_model=cl_vff \
 --student_model=cl_vcnn \
---student_exp_name=gc_o_std100 \
---teacher_exp_name=gc_o_dtchr100 \
+--student_exp_name=gc_f_std101 \
+--teacher_exp_name=gc_o_tchr101 \
 --teacher_config=ff_mnist4 \
 --student_config=vcnn_mnist7 \
---distill_mode=online \
+--distill_mode=offline \
 --batch_size=128 \
 --keep_some_checkpoints=True \
 --max_checkpoints=15 \
---distill_config=pure_dstl2_4_crs_slw_3 > o_run1 &
+--distill_config=pure_dstl5_4_crs_slw_3 > o_run1 &
+
 
 CUDA_VISIBLE_DEVICES=5 python distill/distill_main.py \
 --task=mnist \
 --teacher_model=cl_vff \
 --student_model=cl_vcnn \
---student_exp_name=gc_o_std101 \
---teacher_exp_name=gc_o_tchr101 \
+--student_exp_name=gc_o_std102 \
+--teacher_exp_name=gc_o_tchr102 \
 --teacher_config=ff_mnist4 \
 --student_config=vcnn_mnist7 \
 --distill_mode=online \
@@ -305,9 +320,20 @@ CUDA_VISIBLE_DEVICES=5 python distill/distill_main.py \
 --keep_some_checkpoints=True \
 --max_checkpoints=15 \
 --distill_config=pure_dstl2_4_crs_slw_3 > o_run1 &
-
-
-
+#
+#CUDA_VISIBLE_DEVICES=4 python distill/distill_main.py \
+#--task=mnist \
+#--teacher_model=cl_vff \
+#--student_model=cl_vcnn \
+#--student_exp_name=gc_o_std103 \
+#--teacher_exp_name=gc_o_tchr103 \
+#--teacher_config=ff_mnist4 \
+#--student_config=vcnn_mnist7 \
+#--distill_mode=online \
+#--batch_size=128 \
+#--keep_some_checkpoints=True \
+#--max_checkpoints=15 \
+#--distill_config=pure_dstl2_4_crs_slw_3 > o_run1 &
 
 
 wait
