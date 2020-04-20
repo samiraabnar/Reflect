5a7c73c67131149ee91624b338efc26e22df4888
Mostafa Dehghani <dehghani.mostafa@gmail.com>
2020-04-20 14:53:51 +0200

    vanilaa cnn runs

--- gscripts/gc_mnist.sh
+++ gscripts/gc_mnist.sh
@@ -264,19 +264,19 @@ CUDA_VISIBLE_DEVICES=0 python distill/distill_main.py \
 --max_checkpoints=15 \
 --distill_config=pure_dstl5_4_crs_slw_3 > o_run1 &
 
-CUDA_VISIBLE_DEVICES=1 python distill/distill_main.py \
---task=mnist \
---teacher_model=cl_vcnn \
---student_model=cl_vcnn \
---student_exp_name=gc_f_std15 \
---teacher_exp_name=gc_o_tchr15 \
---teacher_config=vcnn_mnist7 \
---student_config=vcnn_mnist7 \
---distill_mode=offline \
---batch_size=128 \
---keep_some_checkpoints=True \
---max_checkpoints=15 \
---distill_config=pure_dstl5_4_crs_slw_3 > o_run1 &
+#CUDA_VISIBLE_DEVICES=1 python distill/distill_main.py \
+#--task=mnist \
+#--teacher_model=cl_vcnn \
+#--student_model=cl_vcnn \
+#--student_exp_name=gc_f_std15 \
+#--teacher_exp_name=gc_o_tchr15 \
+#--teacher_config=vcnn_mnist7 \
+#--student_config=vcnn_mnist7 \
+#--distill_mode=offline \
+#--batch_size=128 \
+#--keep_some_checkpoints=True \
+#--max_checkpoints=15 \
+#--distill_config=pure_dstl5_4_crs_slw_3 > o_run1 &
 
 
 CUDA_VISIBLE_DEVICES=2 python distill/distill_main.py \
