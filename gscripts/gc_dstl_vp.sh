8ce925e30499c7eadd7212a6474d2a6a2aa99f15
Mostafa Dehghani <dehghani.mostafa@gmail.com>
2020-04-25 19:44:04 +0200

    new runs

--- gscripts/gc_dstl_vp.sh
+++ gscripts/gc_dstl_vp.sh
@@ -57,12 +57,12 @@ CUDA_VISIBLE_DEVICES=4 python distill/distill_main.py  \
 --task=word_sv_agreement_vp \
 --teacher_model=cl_bert \
 --student_model=cl_gpt2 \
---student_exp_name=gc_f_std112 \
+--student_exp_name=gc_o_std112 \
 --teacher_exp_name=gc_o_tchr112 \
 --teacher_config=small_gpt_v9 \
 --student_config=small_gpt_v9 \
---distill_mode=offline \
---distill_config=pure_dstl5_4_crs_slw > run4 &
+--distill_mode=online \
+--distill_config=pure_dstl_4_crs_slw > run4 &
 
 CUDA_VISIBLE_DEVICES=5 python distill/distill_main.py  \
 --task=word_sv_agreement_vp \
