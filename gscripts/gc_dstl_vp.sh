b6823d181865a66f01e8f7bb8bd1c0e32712606d
Mostafa Dehghani <dehghani.mostafa@gmail.com>
2020-04-28 10:35:12 +0200

    new runs

--- gscripts/gc_dstl_vp.sh
+++ gscripts/gc_dstl_vp.sh
@@ -7,16 +7,16 @@ cd ~/Codes/InDist
 export PYTHONPATH=$PYTHONPATH:/home/dehghani/Codes/InDist
 
 
-#CUDA_VISIBLE_DEVICES=0 python distill/distill_main.py  \
-#--task=word_sv_agreement_vp \
-#--teacher_model=cl_bert \
-#--student_model=cl_bert \
-#--student_exp_name=gc_f_std108 \
-#--teacher_exp_name=gc_o_tchr108 \
-#--teacher_config=small_gpt_v9 \
-#--student_config=small_gpt_v9 \
-#--distill_mode=offline \
-#--distill_config=pure_dstl_4_crs_slw > run0 &
+CUDA_VISIBLE_DEVICES=0 python distill/distill_main.py  \
+--task=word_sv_agreement_vp \
+--teacher_model=cl_bert \
+--student_model=cl_bert \
+--student_exp_name=gc_o_std104 \
+--teacher_exp_name=gc_o_tchr104 \
+--teacher_config=small_gpt_v9 \
+--student_config=small_gpt_v9 \
+--distill_mode=online \
+--distill_config=pure_dstl_4_crs_slw > run0 &
 #
 #CUDA_VISIBLE_DEVICES=1 python distill/distill_main.py  \
 #--task=word_sv_agreement_vp \
@@ -64,62 +64,62 @@ export PYTHONPATH=$PYTHONPATH:/home/dehghani/Codes/InDist
 #--distill_mode=offline \
 #--distill_config=pure_dstl_4_crs_slw > run4 &
 
-CUDA_VISIBLE_DEVICES=0 python distill/distill_main.py  \
+CUDA_VISIBLE_DEVICES=1 python distill/distill_main.py  \
 --task=word_sv_agreement_vp \
 --teacher_model=cl_bert_shared \
 --student_model=cl_bert \
---student_exp_name=gc_o_std126 \
+--student_exp_name=gc_f_std126 \
 --teacher_exp_name=gc_o_tchr126 \
 --teacher_config=small_gpt_v9 \
 --student_config=small_gpt_v9 \
---distill_mode=online \
+--distill_mode=offline \
 --distill_config=pure_dstl_4_crs_slw > run5 &
 
-CUDA_VISIBLE_DEVICES=1 python distill/distill_main.py \
+CUDA_VISIBLE_DEVICES=2 python distill/distill_main.py \
 --task=word_sv_agreement_vp \
 --teacher_model=cl_bert_shared \
 --student_model=cl_bert \
---student_exp_name=gc_o_std127 \
+--student_exp_name=gc_f_std127 \
 --teacher_exp_name=gc_o_tchr127 \
 --teacher_config=small_gpt_v9 \
 --student_config=small_gpt_v9 \
---distill_mode=online \
+--distill_mode=offline \
 --distill_config=pure_dstl_4_crs_slw > run6 &
 
-
-CUDA_VISIBLE_DEVICES=2 python distill/distill_main.py  \
---task=word_sv_agreement_vp \
---teacher_model=cl_bert_shared \
---student_model=cl_bert \
---student_exp_name=gc_o_std128 \
---teacher_exp_name=gc_o_tchr128 \
---teacher_config=small_gpt_v9 \
---student_config=small_gpt_v9 \
---distill_mode=online \
---distill_config=pure_dstl_4_crs_slw > run7 &
 
 CUDA_VISIBLE_DEVICES=3 python distill/distill_main.py  \
 --task=word_sv_agreement_vp \
 --teacher_model=cl_bert_shared \
 --student_model=cl_bert \
---student_exp_name=gc_o_std129 \
+--student_exp_name=gc_f_std128 \
+--teacher_exp_name=gc_o_tchr128 \
+--teacher_config=small_gpt_v9 \
+--student_config=small_gpt_v9 \
+--distill_mode=offline \
+--distill_config=pure_dstl_4_crs_slw > run7 &
+
+CUDA_VISIBLE_DEVICES=4 python distill/distill_main.py  \
+--task=word_sv_agreement_vp \
+--teacher_model=cl_bert_shared \
+--student_model=cl_bert \
+--student_exp_name=gc_f_std129 \
 --teacher_exp_name=gc_o_tchr129 \
 --teacher_config=small_gpt_v9 \
 --student_config=small_gpt_v9 \
---distill_mode=online \
+--distill_mode=offline \
 --distill_config=pure_dstl_4_crs_slw > run8 &
 
 
-#CUDA_VISIBLE_DEVICES=4 python distill/distill_main.py  \
-#--task=word_sv_agreement_vp \
-#--teacher_model=cl_lstm \
-#--student_model=cl_lstm \
-#--student_exp_name=gc_f_std124 \
-#--teacher_exp_name=gc_o_tchr124 \
-#--teacher_config=small_lstm_v4 \
-#--student_config=small_lstm_v4 \
-#--distill_mode=offline \
-#--distill_config=pure_dstl_4_crs_slw > run3 &
+CUDA_VISIBLE_DEVICES=5 python distill/distill_main.py  \
+--task=word_sv_agreement_vp \
+--teacher_model=cl_lstm \
+--student_model=cl_lstm \
+--student_exp_name=gc_o_std130 \
+--teacher_exp_name=gc_o_tchr130 \
+--teacher_config=small_lstm_v4 \
+--student_config=small_lstm_v4 \
+--distill_mode=online \
+--distill_config=pure_dstl_4_crs_slw > run3 &
 #
 #CUDA_VISIBLE_DEVICES=5 python distill/distill_main.py  \
 #--task=word_sv_agreement_vp \
