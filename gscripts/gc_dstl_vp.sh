547588da1e9f95ff5c1c313b526d532396a27811
Mostafa Dehghani <dehghani.mostafa@gmail.com>
2020-05-02 22:15:27 +0200

    new runs

--- gscripts/gc_dstl_vp.sh
+++ gscripts/gc_dstl_vp.sh
@@ -291,28 +291,28 @@ export PYTHONPATH=$PYTHONPATH:/home/dehghani/Codes/InDist
 #--distill_mode=offline \
 #--distill_config=pure_dstl_4_crs_slw > run4 &
 
-CUDA_VISIBLE_DEVICES=7 python distill/distill_main.py  \
---task=word_sv_agreement_vp \
---teacher_model=cl_lstm \
---student_model=cl_bert \
---student_exp_name=gc_o_std172 \
---teacher_exp_name=gc_o_tchr12 \
---teacher_config=small_lstm_v4 \
---student_config=small_gpt_v9 \
---distill_mode=online \
---distill_config=pure_dstl_4_crs_slw > run4 &
-
-
-#CUDA_VISIBLE_DEVICES=0 python distill/distill_main.py  \
+#CUDA_VISIBLE_DEVICES=7 python distill/distill_main.py  \
 #--task=word_sv_agreement_vp \
-#--teacher_model=cl_gpt2 \
+#--teacher_model=cl_lstm \
 #--student_model=cl_bert \
-#--student_exp_name=gc_o_std173 \
-#--teacher_exp_name=gc_o_tchr173 \
-#--teacher_config=small_gpt_v9 \
+#--student_exp_name=gc_o_std172 \
+#--teacher_exp_name=gc_o_tchr12 \
+#--teacher_config=small_lstm_v4 \
 #--student_config=small_gpt_v9 \
 #--distill_mode=online \
-#--distill_config=pure_dstl_4_crs_slw > run7 &
+#--distill_config=pure_dstl_4_crs_slw > run4 &
+
+
+CUDA_VISIBLE_DEVICES=0 python distill/distill_main.py  \
+--task=word_sv_agreement_vp \
+--teacher_model=cl_gpt2 \
+--student_model=cl_bert \
+--student_exp_name=gc_o_std173 \
+--teacher_exp_name=gc_o_tchr173 \
+--teacher_config=small_gpt_v9 \
+--student_config=small_gpt_v9 \
+--distill_mode=online \
+--distill_config=pure_dstl_4_crs_slw > run7 &
 #
 #CUDA_VISIBLE_DEVICES=1 python distill/distill_main.py  \
 #--task=word_sv_agreement_vp \
