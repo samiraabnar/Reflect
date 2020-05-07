7c04213119041095d35f8e66c121286742bd98a6
Mostafa Dehghani <dehghani.mostafa@gmail.com>
2020-05-07 09:17:25 +0200

    new runs

--- util/distill_params.py
+++ util/distill_params.py
@@ -662,7 +662,7 @@ pure_dstl_4_exp_slw_vp6 = {
 'teacher_hold_base_rate_steps' : 1000,
 'teacher_optimizer' : 'adam',
 'schedule': 'exp',
-'n_epochs': 30
+'n_epochs': 40
 }
 
 pure_dstl_4_crs_slw_vp7 = {
@@ -740,6 +740,44 @@ pure_dstl_4_exp_slw_vp9 = {
 'teacher_optimizer' : 'adam',
 'schedule': 'exp',
 'n_epochs': 30
+}
+
+pure_dstl_4_exp_vp3 = {
+'distill_temp' : 1.0,
+'student_distill_rate' : 1.0,
+'student_gold_rate' : 0.0,
+'student_learning_rate' : 0.001,
+'student_decay_steps' : 10000,
+'student_decay_rate': 0.7,
+'student_hold_base_rate_steps' :  0,
+'student_warmup_steps' : 0,
+'student_optimizer' : 'adam',
+'teacher_learning_rate' : 0.001,
+'teacher_decay_steps' : 1000,
+'teacher_decay_rate': 0.9,
+'teacher_hold_base_rate_steps' : 1000,
+'teacher_optimizer' : 'adam',
+'schedule': 'exp',
+'n_epochs': 40
+}
+
+pure_dstl_4_exp_vp4 = {
+'distill_temp' : 1.0,
+'student_distill_rate' : 1.0,
+'student_gold_rate' : 0.0,
+'student_learning_rate' : 0.001,
+'student_decay_steps' : 10000,
+'student_decay_rate': 0.8,
+'student_hold_base_rate_steps' :  0,
+'student_warmup_steps' : 0,
+'student_optimizer' : 'adam',
+'teacher_learning_rate' : 0.001,
+'teacher_decay_steps' : 1000,
+'teacher_decay_rate': 0.8,
+'teacher_hold_base_rate_steps' : 1000,
+'teacher_optimizer' : 'adam',
+'schedule': 'exp',
+'n_epochs': 40
 }
 
 pure_dstl_4_crs_slw_hld31 = {
@@ -2061,5 +2099,7 @@ DISTILL_PARAMS = {'pure_dstl_1' :  pure_dstl_1,
                   'pure_dstl_4_crs_slw_vp8': pure_dstl_4_crs_slw_vp8,
                   'pure_dstl_4_crs_slw_vp9': pure_dstl_4_crs_slw_vp9,
                   'pure_dstl_4_exp_slw_vp6': pure_dstl_4_exp_slw_vp6,
-                  'pure_dstl_4_exp_slw_vp9': pure_dstl_4_exp_slw_vp9
+                  'pure_dstl_4_exp_slw_vp9': pure_dstl_4_exp_slw_vp9,
+                  'pure_dstl_4_exp_vp3': pure_dstl_4_exp_vp3,
+                  'pure_dstl_4_exp_vp4': pure_dstl_4_exp_vp4
                   }
