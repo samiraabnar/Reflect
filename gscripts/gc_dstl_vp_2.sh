975fce65c2c9bbb6fae44f7ea4e97bf1ae8e57a1
Mostafa Dehghani <dehghani.mostafa@gmail.com>
2020-05-07 20:10:27 +0200

    new runs

--- gscripts/gc_dstl_vp_2.sh
+++ gscripts/gc_dstl_vp_2.sh
@@ -7,38 +7,38 @@ cd ~/Codes/InDist
 export PYTHONPATH=$PYTHONPATH:/home/dehghani/Codes/InDist
 
 
-CUDA_VISIBLE_DEVICES=0 python distill/distill_main.py  \
---task=word_sv_agreement_vp \
---teacher_model=cl_gpt2_shared \
---student_model=cl_lstm \
---student_exp_name=gc_f_std4101 \
---teacher_exp_name=gc_o_tch4101 \
---teacher_config=small_ugpt_v9 \
---student_config=small_lstm_v4 \
---distill_mode=offline \
---distill_config=pure_dstl_4_exp_vp4 > run0 &
-
-CUDA_VISIBLE_DEVICES=1 python distill/distill_main.py  \
---task=word_sv_agreement_vp \
---teacher_model=cl_gpt2_shared \
---student_model=cl_lstm \
---student_exp_name=gc_o_std4102 \
---teacher_exp_name=gc_o_tchr4102 \
---teacher_config=small_ugpt_v9 \
---student_config=small_lstm_v4 \
---distill_mode=offline \
---distill_config=pure_dstl_4_exp_vp4 > run0 &
-
-#CUDA_VISIBLE_DEVICES=2 python distill/distill_main.py  \
+#CUDA_VISIBLE_DEVICES=0 python distill/distill_main.py  \
 #--task=word_sv_agreement_vp \
 #--teacher_model=cl_gpt2_shared \
 #--student_model=cl_lstm \
-#--student_exp_name=gc_f_std4103 \
-#--teacher_exp_name=gc_o_tch4113 \
+#--student_exp_name=gc_f_std4101 \
+#--teacher_exp_name=gc_o_tch4101 \
 #--teacher_config=small_ugpt_v9 \
 #--student_config=small_lstm_v4 \
 #--distill_mode=offline \
 #--distill_config=pure_dstl_4_exp_vp4 > run0 &
+#
+#CUDA_VISIBLE_DEVICES=1 python distill/distill_main.py  \
+#--task=word_sv_agreement_vp \
+#--teacher_model=cl_gpt2_shared \
+#--student_model=cl_lstm \
+#--student_exp_name=gc_o_std4102 \
+#--teacher_exp_name=gc_o_tchr4102 \
+#--teacher_config=small_ugpt_v9 \
+#--student_config=small_lstm_v4 \
+#--distill_mode=offline \
+#--distill_config=pure_dstl_4_exp_vp4 > run0 &
+
+CUDA_VISIBLE_DEVICES=2 python distill/distill_main.py  \
+--task=word_sv_agreement_vp \
+--teacher_model=cl_gpt2_shared \
+--student_model=cl_lstm \
+--student_exp_name=gc_f_std4103 \
+--teacher_exp_name=gc_o_tchr4113 \
+--teacher_config=small_ugpt_v9 \
+--student_config=small_lstm_v4 \
+--distill_mode=offline \
+--distill_config=pure_dstl_4_exp_vp4 > run0 &
 #
 #CUDA_VISIBLE_DEVICES=3 python distill/distill_main.py  \
 #--task=word_sv_agreement_vp \
