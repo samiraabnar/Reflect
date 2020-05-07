4b3116050fb923a0b58bb03b1615b5ea90f2e300
Mostafa Dehghani <dehghani.mostafa@gmail.com>
2020-05-07 23:03:20 +0200

    new runs

--- gscripts/gc_dstl_vp.sh
+++ gscripts/gc_dstl_vp.sh
@@ -55,7 +55,7 @@ export PYTHONPATH=$PYTHONPATH:/home/dehghani/Codes/InDist
 ############################################################
 
 
-CUDA_VISIBLE_DEVICES=0 python distill/distill_main.py  \
+CUDA_VISIBLE_DEVICES=4 python distill/distill_main.py  \
 --task=word_sv_agreement_vp \
 --teacher_model=cl_lstm \
 --student_model=cl_gpt2_shared \
@@ -66,7 +66,7 @@ CUDA_VISIBLE_DEVICES=0 python distill/distill_main.py  \
 --distill_mode=offline \
 --distill_config=pure_dstl_4_exp_vp5 > run0 &
 
-CUDA_VISIBLE_DEVICES=1 python distill/distill_main.py  \
+CUDA_VISIBLE_DEVICES=5 python distill/distill_main.py  \
 --task=word_sv_agreement_vp \
 --teacher_model=cl_lstm \
 --student_model=cl_gpt2_shared \
@@ -77,7 +77,7 @@ CUDA_VISIBLE_DEVICES=1 python distill/distill_main.py  \
 --distill_mode=offline \
 --distill_config=pure_dstl_4_exp_vp5 > run0 &
 
-CUDA_VISIBLE_DEVICES=2 python distill/distill_main.py  \
+CUDA_VISIBLE_DEVICES=6 python distill/distill_main.py  \
 --task=word_sv_agreement_vp \
 --teacher_model=cl_lstm \
 --student_model=cl_gpt2_shared \
@@ -88,7 +88,7 @@ CUDA_VISIBLE_DEVICES=2 python distill/distill_main.py  \
 --distill_mode=offline \
 --distill_config=pure_dstl_4_exp_vp5 > run0 &
 
-CUDA_VISIBLE_DEVICES=3 python distill/distill_main.py  \
+CUDA_VISIBLE_DEVICES=7 python distill/distill_main.py  \
 --task=word_sv_agreement_vp \
 --teacher_model=cl_lstm \
 --student_model=cl_gpt2_shared \
