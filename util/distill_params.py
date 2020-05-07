37ceacda59d6fbe750af99420e94b8effa905e66
Mostafa Dehghani <dehghani.mostafa@gmail.com>
2020-05-07 12:18:33 +0200

    new runs

--- util/distill_params.py
+++ util/distill_params.py
@@ -781,6 +781,25 @@ pure_dstl_4_exp_vp3 = {
 'n_epochs': 40
 }
 
+pure_dstl_4_exp_vp5 = {
+'distill_temp' : 1.0,
+'student_distill_rate' : 1.0,
+'student_gold_rate' : 0.0,
+'student_learning_rate' : 0.001,
+'student_decay_steps' : 10000,
+'student_decay_rate': 0.7,
+'student_hold_base_rate_steps' :  0,
+'student_warmup_steps' : 0,
+'student_optimizer' : 'adam',
+'teacher_learning_rate' : 0.001,
+'teacher_decay_steps' : 1000,
+'teacher_decay_rate': 0.96,
+'teacher_hold_base_rate_steps' : 1000,
+'teacher_optimizer' : 'adam',
+'schedule': 'exp',
+'n_epochs': 40
+}
+
 pure_dstl_4_exp_vp4 = {
 'distill_temp' : 1.0,
 'student_distill_rate' : 1.0,
@@ -2122,5 +2141,6 @@ DISTILL_PARAMS = {'pure_dstl_1' :  pure_dstl_1,
                   'pure_dstl_4_exp_slw_vp9': pure_dstl_4_exp_slw_vp9,
                   'pure_dstl_4_exp_vp3': pure_dstl_4_exp_vp3,
                   'pure_dstl_4_exp_vp4': pure_dstl_4_exp_vp4,
-                  'pure_dstl_4_exp_vp8': pure_dstl_4_exp_vp8
+                  'pure_dstl_4_exp_vp8': pure_dstl_4_exp_vp8,
+                  'pure_dstl_4_exp_vp5': pure_dstl_4_exp_vp5
                   }
